9c6bd25d054e1a7975b7a39a6ea13610cda851d1
Ihsan Faisal <ihsanfaisal@gmail.com>
2013-03-04 22:24:16 +0700

    database versioning: trigger modification

--- app/DoctrineMigrations/Version20130304105749.php
+++ app/DoctrineMigrations/Version20130304105749.php
@@ -9,71 +9,80 @@ use Doctrine\DBAL\Migrations\AbstractMigration, Doctrine\DBAL\Schema\Schema;
 class Version20130304105749 extends AbstractMigration
 {
     private $trigger1 = "DELIMITER $$
-
 CREATE
 TRIGGER `befins_calonpr`
 BEFORE INSERT ON `calon_pembayaran_rutin`
 FOR EACH ROW
 BEGIN
-	SET NEW.waktu_simpan = NOW();
+    SET NEW.waktu_simpan = NOW();
+    SET NEW.waktu_ubah = NOW();
 END
-$$";
+$$
+DELIMITER ;";
 
-    private $trigger2 = "DROP TRIGGER IF EXISTS `befins_calonps`;
-        DELIMITER //
-        CREATE TRIGGER `befins_calonps` BEFORE INSERT ON `calon_pembayaran_sekali`
-         FOR EACH ROW BEGIN
-            SET NEW.waktu_simpan = NOW();
-            SET NEW.waktu_ubah = NOW();
-        END
-        //
-        DELIMITER ;";
+    private $trigger2 = "DELIMITER $$
+CREATE
+TRIGGER `befins_calonps`
+BEFORE INSERT ON `calon_pembayaran_sekali`
+FOR EACH ROW
+BEGIN
+    SET NEW.waktu_simpan = NOW();
+    SET NEW.waktu_ubah = NOW();
+END
+$$
+DELIMITER ;";
 
-    private $trigger3 = "DROP TRIGGER IF EXISTS `befins_calonsiswa`;
-        DELIMITER //
-        CREATE TRIGGER `befins_calonsiswa` BEFORE INSERT ON `calon_siswa`
-         FOR EACH ROW BEGIN
-            SET NEW.waktu_simpan = NOW();
-            SET NEW.waktu_ubah = NOW();
-        END
-        //
-        DELIMITER ;";
+    private $trigger3 = "DELIMITER $$
+CREATE
+TRIGGER `befins_calonsiswa`
+BEFORE INSERT ON `calon_siswa`
+FOR EACH ROW
+BEGIN
+    SET NEW.waktu_simpan = NOW();
+    SET NEW.waktu_ubah = NOW();
+END
+$$
+DELIMITER ;";
 
-    private $trigger4 = "DROP TRIGGER IF EXISTS `beforeinsertkelas`;
-        DELIMITER //
-        CREATE TRIGGER `beforeinsertkelas` BEFORE INSERT ON `kelas`
-         FOR EACH ROW BEGIN
-            SET NEW.kode = CONCAT((SELECT kode FROM tahun WHERE sekolah_id = NEW.sekolah_id AND id = NEW.tahun_id), NEW.kode);
-        END
-        //
-        DELIMITER ;";
+    private $trigger4 = "DELIMITER $$
+CREATE
+TRIGGER `beforeinsertkelas`
+BEFORE INSERT ON `kelas`
+FOR EACH ROW
+BEGIN
+    SET NEW.kode = CONCAT((SELECT kode FROM tahun WHERE sekolah_id = NEW.sekolah_id AND id = NEW.tahun_id), NEW.kode);
+END$$
+DELIMITER ;";
 
-    private $trigger5 = "DROP TRIGGER IF EXISTS `beforeupdatekelas`;
-        DELIMITER //
-        CREATE TRIGGER `beforeupdatekelas` BEFORE UPDATE ON `kelas`
-         FOR EACH ROW BEGIN
-            DECLARE kodetahun VARCHAR(45);
-            DECLARE kodekelas VARCHAR(50);
+    private $trigger5 = "DELIMITER $$
+CREATE
+TRIGGER `beforeupdatekelas`
+BEFORE UPDATE ON `kelas`
+FOR EACH ROW
+BEGIN
+    DECLARE kodetahun VARCHAR(45);
+    DECLARE kodekelas VARCHAR(50);
+    
+    SET kodetahun = (SELECT kode FROM tahun WHERE sekolah_id = NEW.sekolah_id AND id = NEW.tahun_id);
+    SET kodekelas = NEW.kode;
+    SET NEW.kode = IF(LEFT(kodekelas, LENGTH(kodetahun)) = kodetahun, kodekelas, CONCAT(kodetahun, NEW.kode));
+END$$
+DELIMITER ;";
 
-            SET kodetahun = (SELECT kode FROM tahun WHERE sekolah_id = NEW.sekolah_id AND id = NEW.tahun_id);
-            SET kodekelas = NEW.kode;
-            SET NEW.kode = IF(LEFT(kodekelas, LENGTH(kodetahun)) = kodetahun, kodekelas, CONCAT(kodetahun, NEW.kode));
-        END
-        //
-        DELIMITER ;";
-
-    private $trigger6 = "DROP TRIGGER IF EXISTS `beforeinsertsiswa`;
-        DELIMITER //
-        CREATE TRIGGER `beforeinsertsiswa` BEFORE INSERT ON `siswa`
-         FOR EACH ROW BEGIN
-            DECLARE nomorurutpersekolah INT;
-
-            SET nomorurutpersekolah = (SELECT MAX(nomor_urut_persekolah) FROM siswa WHERE sekolah_id = NEW.sekolah_id);
-            SET NEW.nomor_urut_persekolah = IFNULL(nomorurutpersekolah,100000) + 1;
-            SET NEW.nomor_induk_sistem = CONCAT(CAST(NEW.nomor_urut_persekolah AS CHAR(6)), NEW.sekolah_id);
-        END
-        //
-        DELIMITER ;";
+    private $trigger6 = "DELIMITER $$
+CREATE
+DEFINER=`root`@`localhost`
+TRIGGER `beforeinsertsiswa`
+BEFORE INSERT ON `siswa`
+FOR EACH ROW
+BEGIN
+    DECLARE nomorurutpersekolah INT;
+    
+    SET nomorurutpersekolah = (SELECT MAX(nomor_urut_persekolah) FROM siswa WHERE sekolah_id = NEW.sekolah_id);
+    SET NEW.nomor_urut_persekolah = IFNULL(nomorurutpersekolah,100000) + 1;
+    SET NEW.nomor_induk_sistem = CONCAT(CAST(NEW.nomor_urut_persekolah AS CHAR(6)), NEW.sekolah_id);
+END$$
+DELIMITER ;";
 
     public function up(Schema $schema) {
         // this up() migration is auto-generated, please modify it to your needs
@@ -1426,6 +1435,6 @@ DEFAULT CHARACTER SET = utf8;");
         $this->addSql("DROP TRIGGER IF EXISTS `befins_calonsiswa`;");
         $this->addSql("DROP TRIGGER IF EXISTS `beforeinsertkelas`;");
         $this->addSql("DROP TRIGGER IF EXISTS `beforeupdatekelas`;");
-        $this->addSql("DROP TRIGGER IF EXISTS `beforeinsertsiswa`");
+        $this->addSql("DROP TRIGGER IF EXISTS `beforeinsertsiswa`;");
     }
 }
