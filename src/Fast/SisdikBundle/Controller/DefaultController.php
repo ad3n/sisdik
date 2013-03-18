30dee435b617f1a33fb18a421baf0ac7a3ebebd7
Ihsan Faisal <ihsanfaisal@gmail.com>
2013-03-18 09:54:21 +0700

    display active year in dashboard/homepage

--- src/Fast/SisdikBundle/Controller/DefaultController.php
+++ src/Fast/SisdikBundle/Controller/DefaultController.php
@@ -1,17 +1,45 @@
 <?php
 
 namespace Fast\SisdikBundle\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Fast\SisdikBundle\Entity\Sekolah;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    
+
     /**
      * @Template()
      */
     public function indexAction() {
-        // check school id. if not defined, disabled all?
-        return array();
+        $sekolah = $this->getSchool();
+
+        $em = $this->getDoctrine()->getManager();
+
+        if ($sekolah) {
+            $querybuilder = $em->createQueryBuilder()->select('t')->from('FastSisdikBundle:Tahun', 't')
+                    ->where('t.sekolah = :sekolah')->setParameter('sekolah', $sekolah);
+            $results = $querybuilder->getQuery()->getResult();
+            foreach ($results as $entity) {
+                $tahunaktif = $entity->getNama();
+            }
+        } else {
+            $tahunaktif = null;
+        }
+
+        return array(
+            'tahunaktif' => $tahunaktif
+        );
+    }
+
+    private function getSchool() {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $sekolah = $user->getSekolah();
+
+        if (is_object($sekolah) && $sekolah instanceof Sekolah) {
+            return $sekolah;
+        } else {
+            return false;
+        }
     }
 }
