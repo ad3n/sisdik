c664499bc52dd41b62f8d8242601a02c9e3a176b
Ihsan Faisal <ihsanfaisal@gmail.com>
2013-03-27 14:14:55 +0700

    create script to set up directories access control list and permission

---
+++ setfacl.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "this script require root access with sudo"
+
+sudo setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs
+sudo setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs
---
+++ setpermission.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+chmod 777 app/documents/output
+chmod 777 app/fingerprintlogs
+chmod 777 app/receipts
+chmod 777 app/schedules
+chmod 777 web/uploads
+chmod 777 web/uploads/students
+chmod 777 web/uploads/students/photos
+chmod 777 web/uploads/students/webcam-photos
