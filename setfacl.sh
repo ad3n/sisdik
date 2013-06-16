a8c453bb72059c00c20cbf53d14fbdc985a3e0fb
Ihsan Faisal <ihsanfaisal@gmail.com>
2013-06-17 00:51:49 +0700

    setfacl.sh dan setpermission.sh tidak diperlukan lagi karena sudah diotomatisasi oleh capifony

--- setfacl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root"
-   exit 1
-fi
-
-sudo setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs
-sudo setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs
