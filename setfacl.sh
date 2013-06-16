9631d64bfb2308e9c22f29cb5a80be50246cd801
Ihsan Faisal <ihsanfaisal@gmail.com>
2013-06-16 17:46:12 +0700

    memperbaiki script setfacl.sh

--- setfacl.sh
+++ setfacl.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-
-echo "this script require root access with sudo"
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
 
 sudo setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs
 sudo setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs
