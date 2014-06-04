bf3b2dacd1c5b6fe0ae20271e34fb0d4d736be40
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-06-04 11:48:26 +0700

    Perintah console untuk inisiasi kehadiran, beda waktu maks 610 detik

--- src/Fast/SisdikBundle/Command/InisiasiKehadiranCommand.php
+++ src/Fast/SisdikBundle/Command/InisiasiKehadiranCommand.php
@@ -1,8 +1,7 @@
 <?php
-
 namespace Fast\SisdikBundle\Command;
-use Fast\SisdikBundle\Entity\KalenderPendidikan;
 
+use Fast\SisdikBundle\Entity\KalenderPendidikan;
 use Fast\SisdikBundle\Entity\ProsesKehadiranSiswa;
 use Fast\SisdikBundle\Entity\Sekolah;
 use Fast\SisdikBundle\Entity\SiswaKelas;
@@ -10,6 +9,7 @@ use Fast\SisdikBundle\Entity\KehadiranSiswa;
 use Fast\SisdikBundle\Entity\JadwalKehadiran;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -20,13 +20,19 @@ class InisiasiKehadiranCommand extends ContainerAwareCommand
 {
     const LOCK_FILE = "inisiasi-kehadiran.lock";
     const LOCK_DIR = "lock";
+    const BEDA_WAKTU_MAKS = 610;
 
-    protected function configure() {
-        $this->setName('sisdik:kehadiran:inisiasi')
-                ->setDescription('Periksa dan jalankan inisiasi kehadiran.');
+    protected function configure()
+    {
+        $this
+            ->setName('sisdik:kehadiran:inisiasi')
+            ->setDescription('Periksa dan jalankan inisiasi kehadiran.')
+            ->addOption('paksa', null, InputOption::VALUE_NONE, 'Memaksa pembuatan data awal hari ini')
+        ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
         $em = $this->getContainer()->get('doctrine')->getManager();
 
         $text = '';
@@ -37,121 +43,178 @@ class InisiasiKehadiranCommand extends ContainerAwareCommand
         $mingguanHariKe = $mingguanHariKe - 1 == -1 ? 7 : $mingguanHariKe - 1;
         $bulananHariKe = $waktuSekarang->format('j');
 
-        $debug = true;
-        if ($debug) {
+        if ($input->getOption('paksa')) {
             $jam = '05:00:00';
+            $waktuSekarang = new \DateTime(date("Y-m-d $jam"));
             $mingguanHariKe = 0; // 0 = senin
             $bulananHariKe = 1;
 
-            print
-                    "debug: periksa jadwal jam:$jam, mingguanHariKe:$mingguanHariKe, bulananHariKe:$bulananHariKe"
-                            . "\n";
+            print "[paksa]: periksa jadwal jam:$jam, mingguanHariKe:$mingguanHariKe, bulananHariKe:$bulananHariKe\n";
         }
 
-        $qbSekolah = $em->createQueryBuilder()->select('sekolah')
-                ->from('FastSisdikBundle:Sekolah', 'sekolah');
-        $entitiesSekolah = $qbSekolah->getQuery()->getResult();
-        foreach ($entitiesSekolah as $sekolah) {
+        $qbSekolah = $em->createQueryBuilder()
+            ->select('sekolah')
+            ->from('FastSisdikBundle:Sekolah', 'sekolah')
+        ;
+        $semuaSekolah = $qbSekolah->getQuery()->getResult();
+
+        foreach ($semuaSekolah as $sekolah) {
             if (!(is_object($sekolah) && $sekolah instanceof Sekolah)) {
                 continue;
             }
 
+            if ($input->getOption('paksa')) {
+                print "[paksa]: " . $sekolah->getNama() . "\n";
+            }
+
             $kalenderPendidikan = $em->getRepository('FastSisdikBundle:KalenderPendidikan')
-                    ->findOneBy(
-                            array(
-                                'sekolah' => $sekolah, 'tanggal' => $waktuSekarang, 'kbm' => true,
-                            ));
+                ->findOneBy([
+                    'sekolah' => $sekolah,
+                    'tanggal' => $waktuSekarang,
+                    'kbm' => true,
+                ])
+            ;
             if (!(is_object($kalenderPendidikan) && $kalenderPendidikan instanceof KalenderPendidikan)) {
                 continue;
             }
 
             if (!$this->isLocked($sekolah->getNomorUrut())) {
                 foreach ($perulangan as $key => $value) {
-                    $querybuilder = $em->createQueryBuilder()->select('jadwal')
-                            ->from('FastSisdikBundle:JadwalKehadiran', 'jadwal')
-                            ->leftJoin('jadwal.tahunAkademik', 'tahunAkademik')
-                            ->andWhere('jadwal.sekolah = :sekolah')
-                            ->andWhere('jadwal.paramstatusDariJam = :jam')
-                            ->andWhere('jadwal.perulangan = :perulangan')
-                            ->andWhere('jadwal.permulaan = :permulaan')
-                            ->andWhere('tahunAkademik.aktif = :aktif')
-                            ->setParameter('sekolah', $sekolah->getId())->setParameter('jam', $jam)
-                            ->setParameter('perulangan', $key)->setParameter('permulaan', true)
-                            ->setParameter('aktif', true);
+                    $querybuilder = $em->createQueryBuilder()
+                        ->select('jadwal')
+                        ->from('FastSisdikBundle:JadwalKehadiran', 'jadwal')
+                        ->leftJoin('jadwal.tahunAkademik', 'tahunAkademik')
+                        ->andWhere('jadwal.sekolah = :sekolah')
+                        ->andWhere('jadwal.paramstatusDariJam <= :jam')
+                        ->andWhere('jadwal.perulangan = :perulangan')
+                        ->andWhere('jadwal.permulaan = :permulaan')
+                        ->andWhere('tahunAkademik.aktif = :aktif')
+                        ->setParameter('sekolah', $sekolah)
+                        ->setParameter('jam', $jam)
+                        ->setParameter('perulangan', $key)
+                        ->setParameter('permulaan', true)
+                        ->setParameter('aktif', true)
+                    ;
+
                     if ($key == 'b-mingguan') {
-                        $querybuilder->andWhere('jadwal.mingguanHariKe = :harike')
-                                ->setParameter('harike', $mingguanHariKe);
+                        $querybuilder
+                            ->andWhere('jadwal.mingguanHariKe = :harike')
+                            ->setParameter('harike', $mingguanHariKe)
+                        ;
                     } elseif ($key == 'c-bulanan') {
-                        $querybuilder->andWhere('jadwal.bulananHariKe = :tanggalke')
-                                ->setParameter('tanggalke', $bulananHariKe);
+                        $querybuilder
+                            ->andWhere('jadwal.bulananHariKe = :tanggalke')
+                            ->setParameter('tanggalke', $bulananHariKe)
+                        ;
                     }
-                    $entities = $querybuilder->getQuery()->getResult();
-                    foreach ($entities as $jadwal) {
+
+                    $jadwalKehadiran = $querybuilder->getQuery()->getResult();
+
+                    foreach ($jadwalKehadiran as $jadwal) {
                         if (!(is_object($jadwal) && $jadwal instanceof JadwalKehadiran)) {
                             continue;
                         }
 
-                        $qbSiswaKelas = $em->createQueryBuilder()->select('siswaKelas')
+                        $proses = $em->getRepository('FastSisdikBundle:ProsesKehadiranSiswa')
+                            ->findOneBy([
+                                'sekolah' => $sekolah,
+                                'tahunAkademik' => $jadwal->getTahunAkademik(),
+                                'kelas' => $jadwal->getKelas(),
+                                'tanggal' => $waktuSekarang,
+                            ])
+                        ;
+                        if (is_object($proses) && $proses instanceof ProsesKehadiranSiswa) {
+                            if ($proses->isBerhasilInisiasi()) {
+                                continue;
+                            }
+                        }
+
+                        $dariJam = $jadwal->getParamstatusDariJam();
+
+                        if ($input->getOption('paksa')) {
+                            $dariJam = $jam;
+                        }
+
+                        $waktuJadwal = strtotime(date('Y-m-d') . " $dariJam");
+                        $bedaWaktu = $waktuSekarang->getTimestamp() - $waktuJadwal;
+
+                        if ($input->getOption('paksa')) {
+                            print "[paksa]: param status dari jam = " . $jadwal->getParamstatusDariJam() . "\n";
+                            print "[paksa]: waktu jadwal menjadi = " . date("Y-m-d H:i:s", $waktuJadwal) . "\n";
+                            print "[paksa]: waktu sekarang menjadi = " . $waktuSekarang->format("Y-m-d H:i:s") . "\n";
+                            print "[paksa]: beda waktu = " . $bedaWaktu . "\n";
+                        }
+
+                        if ($bedaWaktu >= 0 && $bedaWaktu <= self::BEDA_WAKTU_MAKS) {
+                            $qbSiswaKelas = $em->createQueryBuilder()
+                                ->select('siswaKelas')
                                 ->from('FastSisdikBundle:SiswaKelas', 'siswaKelas')
                                 ->where('siswaKelas.tahunAkademik = :tahunakademik')
                                 ->andWhere('siswaKelas.kelas = :kelas')
                                 ->setParameter('tahunakademik', $jadwal->getTahunAkademik())
-                                ->setParameter('kelas', $jadwal->getKelas());
-                        $entitiesSiswaKelas = $qbSiswaKelas->getQuery()->getResult();
-                        foreach ($entitiesSiswaKelas as $siswaKelas) {
-                            if (!(is_object($siswaKelas) && $siswaKelas instanceof SiswaKelas)) {
-                                continue;
-                            }
+                                ->setParameter('kelas', $jadwal->getKelas())
+                            ;
+                            $entitiesSiswaKelas = $qbSiswaKelas->getQuery()->getResult();
 
-                            $qbKehadiran = $em->createQueryBuilder()->select('kehadiran')
+                            foreach ($entitiesSiswaKelas as $siswaKelas) {
+                                if (!(is_object($siswaKelas) && $siswaKelas instanceof SiswaKelas)) {
+                                    continue;
+                                }
+
+                                $qbKehadiran = $em->createQueryBuilder()
+                                    ->select('kehadiran')
                                     ->from('FastSisdikBundle:KehadiranSiswa', 'kehadiran')
                                     ->where('kehadiran.sekolah = :sekolah')
                                     ->andWhere('kehadiran.siswa = :siswa')
                                     ->andWhere('kehadiran.tanggal = :tanggal')
                                     ->setParameter('sekolah', $sekolah->getId())
                                     ->setParameter('siswa', $siswaKelas->getSiswa()->getId())
-                                    ->setParameter('tanggal', $waktuSekarang->format('Y-m-d'));
-                            $entityKehadiran = $qbKehadiran->getQuery()->getResult();
-                            if (count($entityKehadiran) >= 1) {
-                                continue;
+                                    ->setParameter('tanggal', $waktuSekarang->format('Y-m-d'))
+                                ;
+                                $entityKehadiran = $qbKehadiran->getQuery()->getResult();
+
+                                if (count($entityKehadiran) >= 1) {
+                                    continue;
+                                }
+
+                                $kehadiran = new KehadiranSiswa();
+                                $kehadiran->setSekolah($jadwal->getSekolah());
+                                $kehadiran->setTahunAkademik($jadwal->getTahunAkademik());
+                                $kehadiran->setKelas($jadwal->getKelas());
+                                $kehadiran->setSiswa($siswaKelas->getSiswa());
+                                $kehadiran->setStatusKehadiran($jadwal->getStatusKehadiran());
+                                $kehadiran->setPermulaan($jadwal->isPermulaan());
+                                $kehadiran->setTanggal($waktuSekarang);
+                                $kehadiran->setJam($jam);
+                                $kehadiran->setSmsTerproses(false);
+
+                                $em->persist($kehadiran);
                             }
 
-                            $kehadiran = new KehadiranSiswa();
-                            $kehadiran->setSekolah($jadwal->getSekolah());
-                            $kehadiran->setTahunAkademik($jadwal->getTahunAkademik());
-                            $kehadiran->setKelas($jadwal->getKelas());
-                            $kehadiran->setSiswa($siswaKelas->getSiswa());
-                            $kehadiran->setStatusKehadiran($jadwal->getStatusKehadiran());
-                            $kehadiran->setPermulaan($jadwal->isPermulaan());
-                            $kehadiran->setTanggal($waktuSekarang);
-                            $kehadiran->setJam($jam);
-                            $kehadiran->setSmsTerproses(false);
+                            $prosesKehadiranSiswa = $em->getRepository('FastSisdikBundle:ProsesKehadiranSiswa')
+                                ->findOneBy([
+                                    'sekolah' => $jadwal->getSekolah()->getId(),
+                                    'tahunAkademik' => $jadwal->getTahunAkademik()->getId(),
+                                    'kelas' => $jadwal->getKelas()->getId(),
+                                    'tanggal' => $waktuSekarang,
+                                ])
+                            ;
 
-                            $em->persist($kehadiran);
+                            if (is_object($prosesKehadiranSiswa) && $prosesKehadiranSiswa instanceof ProsesKehadiranSiswa) {
+                                $prosesKehadiranSiswa->setBerhasilInisiasi(true);
+                            } else {
+                                $prosesKehadiranSiswa = new ProsesKehadiranSiswa();
+                                $prosesKehadiranSiswa->setSekolah($jadwal->getSekolah());
+                                $prosesKehadiranSiswa->setTahunAkademik($jadwal->getTahunAkademik());
+                                $prosesKehadiranSiswa->setKelas($jadwal->getKelas());
+                                $prosesKehadiranSiswa->setTanggal($waktuSekarang);
+                                $prosesKehadiranSiswa->setBerhasilInisiasi(true);
+                            }
+
+                            $em->persist($prosesKehadiranSiswa);
+                            $em->flush();
+
                         }
-
-                        $prosesKehadiranSiswa = $em->getRepository('FastSisdikBundle:ProsesKehadiranSiswa')
-                                ->findOneBy(
-                                        array(
-                                                'sekolah' => $jadwal->getSekolah()->getId(),
-                                                'tahunAkademik' => $jadwal->getTahunAkademik()->getId(),
-                                                'kelas' => $jadwal->getKelas()->getId(),
-                                                'tanggal' => $waktuSekarang,
-                                        ));
-                        if (is_object($prosesKehadiranSiswa)
-                                && $prosesKehadiranSiswa instanceof ProsesKehadiranSiswa) {
-                            $prosesKehadiranSiswa->setBerhasilInisiasi(true);
-                        } else {
-                            $prosesKehadiranSiswa = new ProsesKehadiranSiswa();
-                            $prosesKehadiranSiswa->setSekolah($jadwal->getSekolah());
-                            $prosesKehadiranSiswa->setTahunAkademik($jadwal->getTahunAkademik());
-                            $prosesKehadiranSiswa->setKelas($jadwal->getKelas());
-                            $prosesKehadiranSiswa->setTanggal($waktuSekarang);
-                            $prosesKehadiranSiswa->setBerhasilInisiasi(true);
-                        }
-                        $em->persist($prosesKehadiranSiswa);
-
-                        $em->flush();
                     }
                 }
 
@@ -159,23 +222,28 @@ class InisiasiKehadiranCommand extends ContainerAwareCommand
                     $output->writeln($text);
                 }
             } else {
-                print
-                        "proses inisiasi kehadiran sekolah " . $sekolah->getNama()
-                                . " telah dan sedang berjalan\n";
+                print "proses inisiasi kehadiran sekolah " . $sekolah->getNama() . " telah dan sedang berjalan\n";
             }
         }
     }
 
     /**
      * Memeriksa apakah proses sebelumnya sedang berjalan
-     * yang ditandai dengan terkunci lock file
+     * yang ditandai dengan kuncian file lock
      *
      * @param  int     $nomorUrutSekolah
      * @return boolean
      */
-    private function isLocked($nomorUrutSekolah) {
-        $lockfile = $this->getContainer()->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR . self::LOCK_DIR
-                . DIRECTORY_SEPARATOR . $nomorUrutSekolah . '.' . self::LOCK_FILE;
+    private function isLocked($nomorUrutSekolah)
+    {
+        $lockfile = $this->getContainer()->get('kernel')->getRootDir()
+            . DIRECTORY_SEPARATOR
+            . self::LOCK_DIR
+            . DIRECTORY_SEPARATOR
+            . $nomorUrutSekolah
+            . '.'
+            . self::LOCK_FILE
+        ;
 
         if (file_exists($lockfile)) {
             $lockingPID = trim(file_get_contents($lockfile));
@@ -185,7 +253,7 @@ class InisiasiKehadiranCommand extends ContainerAwareCommand
             if (in_array($lockingPID, $pids))
                 return true;
 
-            print "Removing stale lock file.\n";
+            print "Removing stale $nomorUrutSekolah.inisiasi-kehadiran.lock file.\n";
             unlink($lockfile);
         }
 
