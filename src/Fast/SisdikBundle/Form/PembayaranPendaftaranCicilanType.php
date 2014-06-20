108e7cb9b36007ace28c131801ee9cd250497e0a
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-06-20 18:48:02 +0700

    Merapikan kode form dan menjadikannya service 8

--- src/Fast/SisdikBundle/Form/PembayaranPendaftaranCicilanType.php
+++ src/Fast/SisdikBundle/Form/PembayaranPendaftaranCicilanType.php
@@ -6,13 +6,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Fast\SisdikBundle\Entity\BiayaPendaftaran;
+use JMS\DiExtraBundle\Annotation\FormType;
 
+/**
+ * @FormType
+ */
 class PembayaranPendaftaranCicilanType extends AbstractType
 {
-
+    /**
+     * @var ContainerInterface
+     */
     private $container;
 
+    /**
+     * @param ContainerInterface $container
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
@@ -20,31 +28,34 @@ class PembayaranPendaftaranCicilanType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $user = $this->container->get('security.context')
-            ->getToken()
-            ->getUser();
-        $builder->add('transaksiPembayaranPendaftaran', 'collection', array(
-            'type' => new TransaksiPembayaranPendaftaranType($this->container),
-            'by_reference' => false,
-            'attr' => array(
-                'class' => 'large'
-            ),
-            'label' => 'label.fee.registration.transaction',
-            'options' => array(
-                'widget_form_group' => false,
-                'label_render' => false
-            ),
-            'label_render' => false,
-            'allow_add' => true,
-            'allow_delete' => false,
-        ));
+        $user = $this->container->get('security.context')->getToken()->getUser();
+
+        $builder
+            ->add('transaksiPembayaranPendaftaran', 'collection', [
+                'type' => new TransaksiPembayaranPendaftaranType($this->container),
+                'by_reference' => false,
+                'attr' => [
+                    'class' => 'large',
+                ],
+                'label' => 'label.fee.registration.transaction',
+                'options' => [
+                    'widget_form_group' => false,
+                    'label_render' => false,
+                ],
+                'label_render' => false,
+                'allow_add' => true,
+                'allow_delete' => false,
+            ])
+        ;
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'Fast\SisdikBundle\Entity\PembayaranPendaftaran'
-        ));
+        $resolver
+            ->setDefaults([
+                'data_class' => 'Fast\SisdikBundle\Entity\PembayaranPendaftaran',
+            ])
+        ;
     }
 
     public function getName()
