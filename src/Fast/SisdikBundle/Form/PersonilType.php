87e8709944d5006dcd4724507397b27037c8e730
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-06-20 18:56:31 +0700

    Merapikan kode form dan menjadikannya service 9

--- src/Fast/SisdikBundle/Form/PersonilType.php
+++ src/Fast/SisdikBundle/Form/PersonilType.php
@@ -1,43 +1,50 @@
 <?php
-
 namespace Fast\SisdikBundle\Form;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use JMS\DiExtraBundle\Annotation\FormType;
 
+/**
+ * @FormType
+ */
 class PersonilType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $builder
-                ->add('id', 'hidden',
-                        array(
-                                'label_render' => false, 'required' => false,
-                                'attr' => array(
-                                    'class' => 'id-panitia'
-                                ),
-                        ));
-
-        $builder
-                ->add('user', 'text',
-                        array(
-                                'label' => 'label.username', 'required' => false,
-                                'attr' => array(
-                                        'class' => 'xlarge committee-username ketik-pilih-tambah',
-                                        'placeholder' => 'label.username',
-                                ), 'label_render' => false,
-                                'horizontal' => false,
-                        ));
+            ->add('id', 'hidden', [
+                'label_render' => false,
+                'required' => false,
+                'attr' => [
+                    'class' => 'id-panitia',
+                ],
+            ])
+            ->add('user', 'text', [
+                'label' => 'label.username',
+                'required' => false,
+                'attr' => [
+                    'class' => 'xlarge committee-username ketik-pilih-tambah',
+                    'placeholder' => 'label.username',
+                ],
+                'label_render' => false,
+                'horizontal' => false,
+            ])
+        ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
         $resolver
-                ->setDefaults(
-                        array(
-                            'data_class' => 'Fast\SisdikBundle\Entity\Personil',
-                        ));
+            ->setDefaults([
+                'data_class' => 'Fast\SisdikBundle\Entity\Personil',
+            ])
+        ;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return 'fast_sisdikbundle_personiltype';
     }
 }
