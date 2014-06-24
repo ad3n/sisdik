3a46c5b28a50e042d34aa998d1381ea42b728cfc
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-06-24 10:34:22 +0700

    Merapikan kode form dan menjadikannya service 11

--- src/Fast/SisdikBundle/Form/ReportSummaryType.php
+++ src/Fast/SisdikBundle/Form/ReportSummaryType.php
@@ -1,58 +1,71 @@
 <?php
-
 namespace Fast\SisdikBundle\Form;
+
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use JMS\DiExtraBundle\Annotation\FormType;
 
+/**
+ * @FormType
+ */
 class ReportSummaryType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $builder
-                ->add('output', 'hidden',
-                        array(
-                                'data' => 'pdf',
-                                'attr' => array(
-                                    'class' => 'output-ringkasan'
-                                ), 'required' => true, 'label_render' => false,
-                        ))
-                ->add('teks', 'textarea',
-                        array(
-                                'label' => 'label.teks.ringkasan',
-                                'attr' => array(
-                                    'class' => 'xlarge ringkasan-teks',
-                                ), 'help_block' => 'help.tag.standar.laporan.psb',
-                                'label_attr' => array(
-                                    'class' => 'label-ringkasan-teks'
-                                ), 'required' => true, 'label_render' => true,
-                                'horizontal_label_class' => '',
-                        ))
-                ->add('teksTerformat', 'hidden',
-                        array(
-                                'attr' => array(
-                                    'class' => 'teks-terformat'
-                                ), 'required' => true, 'label_render' => false,
-                        ))
-                ->add('nomorPonsel', 'text',
-                        array(
-                                'label' => 'label.ponsel',
-                                'attr' => array(
-                                    'class' => 'large nomor-ponsel', 'placeholder' => 'label.perlu.untuk.sms',
-                                ), 'required' => false, 'label_render' => true,
-                                'horizontal_label_class' => '',
-                        ));
-
+            ->add('output', 'hidden', [
+                'data' => 'pdf',
+                'attr' => [
+                    'class' => 'output-ringkasan',
+                ],
+                'required' => true,
+                'label_render' => false,
+            ])
+            ->add('teks', 'textarea', [
+                'label' => 'label.teks.ringkasan',
+                'attr' => [
+                    'class' => 'xlarge ringkasan-teks',
+                ],
+                'help_block' => 'help.tag.standar.laporan.psb',
+                'label_attr' => [
+                    'class' => 'label-ringkasan-teks'
+                ],
+                'required' => true,
+                'label_render' => true,
+                'horizontal_label_class' => '',
+            ])
+            ->add('teksTerformat', 'hidden', [
+                'attr' => [
+                    'class' => 'teks-terformat',
+                ],
+                'required' => true,
+                'label_render' => false,
+            ])
+            ->add('nomorPonsel', 'text', [
+                'label' => 'label.ponsel',
+                'attr' => array(
+                    'class' => 'large nomor-ponsel',
+                    'placeholder' => 'label.perlu.untuk.sms',
+                ),
+                'required' => false,
+                'label_render' => true,
+                'horizontal_label_class' => '',
+            ])
+        ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
         $resolver
-                ->setDefaults(
-                        array(
-                            'csrf_protection' => false,
-                        ));
+            ->setDefaults([
+                'csrf_protection' => false,
+            ])
+        ;
     }
 
-    public function getName() {
+    public function getName()
+    {
         return 'siswaapplicantreportsummary';
     }
 }
