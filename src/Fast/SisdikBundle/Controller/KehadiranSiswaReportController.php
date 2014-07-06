20b3d0aa8aabc7d2c6f5c5b56f3d456e725ab1fb
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-07-06 11:24:19 +0700

    Menambah modul laporan kehadiran siswa

---
+++ src/Fast/SisdikBundle/Controller/KehadiranSiswaReportController.php
@@ -0,0 +1,248 @@
+<?php
+namespace Fast\SisdikBundle\Controller;
+
+use Fast\SisdikBundle\Entity\TahunAkademik;
+use Fast\SisdikBundle\Entity\Siswa;
+use Fast\SisdikBundle\Entity\Kelas;
+use Fast\SisdikBundle\Entity\Sekolah;
+use Fast\SisdikBundle\Entity\KehadiranSiswa;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * @Route("/laporan-kehadiran-siswa")
+ * @PreAuthorize("hasRole('ROLE_GURU_PIKET') or hasRole('ROLE_GURU')")
+ */
+class KehadiranSiswaReportController extends Controller
+{
+    const DOCUMENTS_BASEDIR = "/documents/base/";
+    const BASEFILE = "base.ods";
+    const OUTPUTFILE = "laporan-kehadiran-siswa.";
+    const DOCUMENTS_OUTPUTDIR = "uploads/sekolah/laporan-kehadiran/";
+
+    /**
+     * @Route("/", name="laporan-kehadiran-siswa")
+     * @Method("GET")
+     * @Template("FastSisdikBundle:KehadiranSiswa:laporan.html.twig")
+     */
+    public function indexAction()
+    {
+        $sekolah = $this->isRegisteredToSchool();
+        $this->setCurrentMenu();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $searchform = $this->createForm('sisdik_kehadiransiswareportsearch');
+
+        $hariIni = new \DateTime();
+        $searchform->get('hinggaTanggal')->setData($hariIni);
+
+        $tahunAkademik = $em->getRepository('FastSisdikBundle:TahunAkademik')
+            ->findOneBy([
+                'aktif' => true,
+                'sekolah' => $sekolah->getId(),
+            ])
+        ;
+
+        if (!(is_object($tahunAkademik) && $tahunAkademik instanceof TahunAkademik)) {
+            throw $this->createNotFoundException($this->get('translator')->trans('flash.tahun.akademik.tidak.ada.yang.aktif'));
+        }
+
+        return [
+            'searchform' => $searchform->createView(),
+            'tahunAkademik' => $tahunAkademik,
+        ];
+    }
+
+    /**
+     * @Route("/ekspor", name="laporan-kehadiran-siswa_ekspor")
+     * @Method("POST")
+     */
+    public function eksporAction()
+    {
+        $sekolah = $this->isRegisteredToSchool();
+        $this->setCurrentMenu();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $tahunAkademik = $em->getRepository('FastSisdikBundle:TahunAkademik')
+            ->findOneBy([
+                'aktif' => true,
+                'sekolah' => $sekolah->getId(),
+            ])
+        ;
+
+        if (!(is_object($tahunAkademik) && $tahunAkademik instanceof TahunAkademik)) {
+            $return = [
+                "error" => $this->get('translator')->trans('flash.tahun.akademik.tidak.ada.yang.aktif'),
+            ];
+
+            $return = json_encode($return);
+
+            return new Response($return, 200, [
+                'Content-Type' => 'application/json',
+            ]);
+        }
+
+        $querybuilder = $em->createQueryBuilder()
+            ->select('kehadiranSiswa')
+            ->from('FastSisdikBundle:KehadiranSiswa', 'kehadiranSiswa')
+            ->leftJoin('kehadiranSiswa.siswa', 'siswa')
+            ->where('kehadiranSiswa.sekolah = :sekolah')
+            ->andWhere('kehadiranSiswa.tahunAkademik = :tahunAkademik')
+            ->setParameter('sekolah', $sekolah)
+            ->setParameter('tahunAkademik', $tahunAkademik)
+        ;
+
+        $searchform = $this->createForm('sisdik_kehadiransiswareportsearch');
+        $searchform->submit($this->getRequest());
+        $searchdata = $searchform->getData();
+
+        if ($searchform->isValid()) {
+            if ($searchdata['kelas'] instanceof Kelas) {
+                $querybuilder->andWhere('kehadiranSiswa.kelas = :kelas');
+                $querybuilder->setParameter('kelas', $searchdata['kelas']);
+            }
+
+            $dariTanggal = $searchdata['dariTanggal'];
+            $hinggaTanggal = $searchdata['hinggaTanggal'];
+
+            if ($dariTanggal instanceof \DateTime && $hinggaTanggal instanceof \DateTime) {
+                $querybuilder->andWhere('kehadiranSiswa.tanggal >= :dariTanggal AND kehadiranSiswa.tanggal <= :hinggaTanggal');
+                $querybuilder->setParameter('dariTanggal', $dariTanggal->format("Y-m-d 00:00:00"));
+                $querybuilder->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d 24:00:00"));
+                $querybuilder->addOrderBy('kehadiranSiswa.tanggal', 'ASC');
+            } elseif (!($dariTanggal instanceof \DateTime) && $hinggaTanggal instanceof \DateTime) {
+                $querybuilder->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal');
+                $querybuilder->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d"));
+            } else {
+                $hariIni = new \DateTime();
+                $querybuilder->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal');
+                $querybuilder->setParameter('hinggaTanggal', $hariIni->format("Y-m-d"));
+            }
+        }
+
+        $querybuilder
+            ->addOrderBy('siswa.nomorInduk', 'ASC')
+            ->addOrderBy('siswa.namaLengkap', 'ASC')
+        ;
+
+        $kehadiranSiswa = $querybuilder->getQuery()->getResult();
+
+        $documentbase = $this->get('kernel')->getRootDir() . self::DOCUMENTS_BASEDIR . self::BASEFILE;
+        $outputdir = self::DOCUMENTS_OUTPUTDIR;
+
+        $filenameoutput = self::OUTPUTFILE . date("Y-m-d h:i") . ".sisdik";
+
+        $outputfiletype = "ods";
+        $extensiontarget = $extensionsource = ".$outputfiletype";
+        $filesource = $filenameoutput . $extensionsource;
+        $filetarget = $filenameoutput . $extensiontarget;
+
+        $fs = new Filesystem();
+        if (!$fs->exists($outputdir . $sekolah->getId() . '/')) {
+            $fs->mkdir($outputdir . $sekolah->getId() . '/');
+        }
+
+        $documentsource = $outputdir . $sekolah->getId() . '/' . $filesource;
+        $documenttarget = $outputdir . $sekolah->getId() . '/' . $filetarget;
+
+        if ($outputfiletype == 'ods') {
+            if (copy($documentbase, $documenttarget) === TRUE) {
+                $ziparchive = new \ZipArchive();
+                $ziparchive->open($documenttarget);
+                $ziparchive->addFromString('content.xml', $this->renderView("FastSisdikBundle:KehadiranSiswa:laporan.xml.twig", [
+                        'kehadiranSiswa' => $kehadiranSiswa,
+                        'tahunAkademik' => $tahunAkademik,
+                        'kelas' => $searchdata['kelas'],
+                        'dariTanggal' => $searchdata['dariTanggal'],
+                        'hinggaTanggal' => $searchdata['hinggaTanggal'],
+                    ])
+                );
+
+                if ($ziparchive->close() === TRUE) {
+                    $return = [
+                        "redirectUrl" => $this->generateUrl("laporan-kehadiran-siswa_unduh", [
+                            'filename' => $filetarget,
+                        ]),
+                        "filename" => $filetarget,
+                    ];
+
+                    $return = json_encode($return);
+
+                    return new Response($return, 200, [
+                        'Content-Type' => 'application/json',
+                    ]);
+                }
+            }
+        }
+
+        $return = [
+            "error" => $this->get('translator')->trans('errorinfo.tak.ada.kehadiran.siswa'),
+        ];
+
+        $return = json_encode($return);
+
+        return new Response($return, 200, [
+            'Content-Type' => 'application/json',
+        ]);
+    }
+
+    /**
+     * @Route("/unduh/{filename}/{type}", name="laporan-kehadiran-siswa_unduh")
+     * @Method("GET")
+     */
+    public function unduhAction($filename, $type = 'ods')
+    {
+        $sekolah = $this->isRegisteredToSchool();
+
+        $filetarget = $filename;
+        $documenttarget = self::DOCUMENTS_OUTPUTDIR . $sekolah->getId() . '/' . $filetarget;
+
+        $response = new Response(file_get_contents($documenttarget), 200);
+        $doc = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $filetarget);
+        $response->headers->set('Content-Disposition', $doc);
+        $response->headers->set('Content-Description', 'Laporan Pendaftaran');
+
+        if ($type == 'ods') {
+            $response->headers->set('Content-Type', 'application/vnd.oasis.opendocument.spreadsheet');
+        } elseif ($type == 'pdf') {
+            $response->headers->set('Content-Type', 'application/pdf');
+        }
+
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Expires', '0');
+        $response->headers->set('Cache-Control', 'must-revalidate');
+        $response->headers->set('Pragma', 'public');
+        $response->headers->set('Content-Length', filesize($documenttarget));
+
+        return $response;
+    }
+
+    private function setCurrentMenu()
+    {
+        $menu = $this->container->get('fast_sisdik.menu.main');
+        $menu[$this->get('translator')->trans('headings.presence', array(), 'navigations')][$this->get('translator')->trans('links.laporan.kehadiran.siswa', array(), 'navigations')]->setCurrent(true);
+    }
+
+    private function isRegisteredToSchool()
+    {
+        $user = $this->getUser();
+        $sekolah = $user->getSekolah();
+
+        if (is_object($sekolah) && $sekolah instanceof Sekolah) {
+            return $sekolah;
+        } elseif ($this->container->get('security.context')->isGranted('ROLE_SUPER_ADMIN')) {
+            throw new AccessDeniedException($this->get('translator')->trans('exception.useadmin'));
+        } else {
+            throw new AccessDeniedException($this->get('translator')->trans('exception.registertoschool'));
+        }
+    }
+}
---
+++ src/Fast/SisdikBundle/Form/KehadiranSiswaReportSearchType.php
@@ -0,0 +1,135 @@
+<?php
+namespace Fast\SisdikBundle\Form;
+
+use Doctrine\ORM\EntityManager;
+use Fast\SisdikBundle\Entity\Sekolah;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use JMS\DiExtraBundle\Annotation\FormType;
+use JMS\DiExtraBundle\Annotation\Inject;
+use JMS\DiExtraBundle\Annotation\InjectParams;
+
+/**
+ * @FormType
+ */
+class KehadiranSiswaReportSearchType extends AbstractType
+{
+    /**
+     * @var SecurityContext
+     */
+    private $securityContext;
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @InjectParams({
+     *     "securityContext" = @Inject("security.context"),
+     *     "entityManager" = @Inject("doctrine.orm.entity_manager")
+     * })
+     *
+     * @param SecurityContext $securityContext
+     * @param EntityManager   $entityManager
+     */
+    public function __construct(SecurityContext $securityContext, EntityManager $entityManager)
+    {
+        $this->securityContext = $securityContext;
+        $this->entityManager = $entityManager;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $user = $this->securityContext->getToken()->getUser();
+        $sekolah = $user->getSekolah();
+
+        $em = $this->entityManager;
+
+        $builder
+            ->add('dariTanggal', 'date', [
+                'label' => 'label.date',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'attr' => [
+                    'class' => 'date1 small',
+                    'placeholder' => 'dari.tanggal',
+                ],
+                'required' => false,
+                'label_render' => false,
+                'horizontal' => false,
+            ])
+            ->add('hinggaTanggal', 'date', [
+                'label' => 'label.date',
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'attr' => [
+                    'class' => 'date2 small',
+                    'placeholder' => 'hingga.tanggal',
+                ],
+                'required' => true,
+                'label_render' => false,
+                'horizontal' => false,
+            ])
+        ;
+
+        $querybuilder = $em->createQueryBuilder()
+            ->select('tingkat')
+            ->from('FastSisdikBundle:Tingkat', 'tingkat')
+            ->where('tingkat.sekolah = :sekolah')
+            ->orderBy('tingkat.kode')
+            ->setParameter('sekolah', $sekolah)
+        ;
+        $builder
+            ->add('tingkat', 'entity', [
+                'class' => 'FastSisdikBundle:Tingkat',
+                'label' => 'label.class.entry',
+                'multiple' => false,
+                'expanded' => false,
+                'required' => true,
+                'property' => 'optionLabel',
+                'query_builder' => $querybuilder,
+                'attr' => [
+                    'class' => 'medium pilih-tingkat',
+                ],
+                'label_render' => false,
+                'horizontal' => false,
+            ])
+        ;
+
+        $querybuilder = $em->createQueryBuilder()
+            ->select('kelas')
+            ->from('FastSisdikBundle:Kelas', 'kelas')
+            ->leftJoin('kelas.tingkat', 'tingkat')
+            ->leftJoin('kelas.tahunAkademik', 'tahunAkademik')
+            ->where('kelas.sekolah = :sekolah')
+            ->andWhere('tahunAkademik.aktif = :aktif')
+            ->orderBy('tingkat.urutan', 'ASC')
+            ->addOrderBy('kelas.urutan')
+            ->setParameter('sekolah', $sekolah)
+            ->setParameter('aktif', true)
+        ;
+        $builder
+            ->add('kelas', 'entity', [
+                'class' => 'FastSisdikBundle:Kelas',
+                'label' => 'label.class.entry',
+                'multiple' => false,
+                'expanded' => false,
+                'property' => 'nama',
+                'required' => true,
+                'query_builder' => $querybuilder,
+                'attr' => [
+                    'class' => 'medium pilih-kelas',
+                ],
+                'label_render' => false,
+                'horizontal' => false,
+            ])
+        ;
+    }
+
+    public function getName()
+    {
+        return 'sisdik_kehadiransiswareportsearch';
+    }
+}
