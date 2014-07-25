72d4f66e93ecf5e78abed3f2cd64dc74cc785271
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-07-25 19:16:15 +0700

    Merapikan kode terkait controller ImbalanPendaftaran

--- src/Langgas/SisdikBundle/Controller/ImbalanPendaftaranController.php
+++ src/Langgas/SisdikBundle/Controller/ImbalanPendaftaranController.php
@@ -1,16 +1,14 @@
 <?php
 
 namespace Langgas\SisdikBundle\Controller;
+
 use Doctrine\DBAL\DBALException;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Langgas\SisdikBundle\Entity\ImbalanPendaftaran;
-use Langgas\SisdikBundle\Form\ImbalanPendaftaranType;
-use Langgas\SisdikBundle\Form\SimpleTahunSearchType;
 use Langgas\SisdikBundle\Entity\JenisImbalan;
 use Langgas\SisdikBundle\Entity\Tahun;
 use Langgas\SisdikBundle\Entity\Gelombang;
@@ -18,59 +16,61 @@ use Langgas\SisdikBundle\Entity\Sekolah;
 use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
 
 /**
- * ImbalanPendaftaran controller.
- *
- * @Route("/rewardamount")
+ * @Route("/imbalan-pendaftaran")
  * @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_BENDAHARA')")
  */
 class ImbalanPendaftaranController extends Controller
 {
     /**
-     * Lists all ImbalanPendaftaran entities.
-     *
      * @Route("/", name="rewardamount")
      * @Template()
      */
-    public function indexAction() {
-        $sekolah = $this->isRegisteredToSchool();
+    public function indexAction()
+    {
+        $sekolah = $this->getSekolah();
         $this->setCurrentMenu();
 
         $em = $this->getDoctrine()->getManager();
 
-        $searchform = $this->createForm(new SimpleTahunSearchType($this->container));
+        $searchform = $this->createForm('sisdik_caritahun');
 
-        $querybuilder = $em->createQueryBuilder()->select('t')
-                ->from('LanggasSisdikBundle:ImbalanPendaftaran', 't')->leftJoin('t.tahun', 't2')
-                ->leftJoin('t.gelombang', 't3')->leftJoin('t.jenisImbalan', 't4')
-                ->where('t2.sekolah = :sekolah')->orderBy('t2.tahun', 'DESC')->addOrderBy('t3.urutan', 'ASC');
+        $querybuilder = $em->createQueryBuilder()
+            ->select('imbalanPendaftaran')
+            ->from('LanggasSisdikBundle:ImbalanPendaftaran', 'imbalanPendaftaran')
+            ->leftJoin('imbalanPendaftaran.tahun', 'tahun')
+            ->leftJoin('imbalanPendaftaran.gelombang', 'gelombang')
+            ->leftJoin('imbalanPendaftaran.jenisImbalan', 't4')
+            ->where('tahun.sekolah = :sekolah')
+            ->orderBy('tahun.tahun', 'DESC')
+            ->addOrderBy('gelombang.urutan', 'ASC')
+        ;
         $querybuilder->setParameter('sekolah', $sekolah->getId());
 
         $searchform->submit($this->getRequest());
         if ($searchform->isValid()) {
             $searchdata = $searchform->getData();
 
-            if ($searchdata['tahun'] != '') {
-                $querybuilder->andWhere('t2.id = :tahun');
-                $querybuilder->setParameter('tahun', $searchdata['tahun']->getId());
+            if ($searchdata['tahun'] instanceof Tahun) {
+                $querybuilder->andWhere('imbalanPendaftaran.tahun = :tahun');
+                $querybuilder->setParameter('tahun', $searchdata['tahun']);
             }
         }
 
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($querybuilder, $this->getRequest()->query->get('page', 1));
 
-        return array(
-            'pagination' => $pagination, 'searchform' => $searchform->createView()
-        );
+        return [
+            'pagination' => $pagination,
+            'searchform' => $searchform->createView(),
+        ];
     }
 
     /**
-     * Finds and displays a ImbalanPendaftaran entity.
-     *
      * @Route("/{id}/show", name="rewardamount_show")
      * @Template()
      */
-    public function showAction($id) {
-        $sekolah = $this->isRegisteredToSchool();
+    public function showAction($id)
+    {
         $this->setCurrentMenu();
 
         $em = $this->getDoctrine()->getManager();
@@ -83,42 +83,42 @@ class ImbalanPendaftaranController extends Controller
 
         $deleteForm = $this->createDeleteForm($id);
 
-        return array(
-            'entity' => $entity, 'delete_form' => $deleteForm->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ];
     }
 
     /**
-     * Displays a form to create a new ImbalanPendaftaran entity.
-     *
      * @Route("/new", name="rewardamount_new")
      * @Template()
      */
-    public function newAction() {
-        $sekolah = $this->isRegisteredToSchool();
+    public function newAction()
+    {
         $this->setCurrentMenu();
 
-        $entity = new ImbalanPendaftaran();
-        $form = $this->createForm(new ImbalanPendaftaranType($this->container), $entity);
+        $entity = new ImbalanPendaftaran;
 
-        return array(
-            'entity' => $entity, 'form' => $form->createView(),
-        );
+        $form = $this->createForm('sisdik_imbalanpendaftaran', $entity);
+
+        return [
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ];
     }
 
     /**
-     * Creates a new ImbalanPendaftaran entity.
-     *
      * @Route("/create", name="rewardamount_create")
      * @Method("POST")
      * @Template("LanggasSisdikBundle:ImbalanPendaftaran:new.html.twig")
      */
-    public function createAction(Request $request) {
-        $sekolah = $this->isRegisteredToSchool();
+    public function createAction(Request $request)
+    {
         $this->setCurrentMenu();
 
-        $entity = new ImbalanPendaftaran();
-        $form = $this->createForm(new ImbalanPendaftaranType($this->container), $entity);
+        $entity = new ImbalanPendaftaran;
+
+        $form = $this->createForm('sisdik_imbalanpendaftaran', $entity);
         $form->submit($request);
 
         if ($form->isValid()) {
@@ -128,36 +128,33 @@ class ImbalanPendaftaranController extends Controller
                 $em->persist($entity);
                 $em->flush();
 
-                $this->get('session')->getFlashBag()
-                        ->add('success', $this->get('translator')->trans('flash.reward.amount.inserted'));
-
+                $this
+                    ->get('session')
+                    ->getFlashBag()
+                    ->add('success', $this->get('translator')->trans('flash.reward.amount.inserted'))
+                ;
             } catch (DBALException $e) {
                 $message = $this->get('translator')->trans('exception.unique.rewardamount');
                 throw new DBALException($message);
             }
 
-            return $this
-                    ->redirect(
-                            $this
-                                    ->generateUrl('rewardamount_show',
-                                            array(
-                                                'id' => $entity->getId()
-                                            )));
+            return $this->redirect($this->generateUrl('rewardamount_show', [
+                'id' => $entity->getId(),
+            ]));
         }
 
-        return array(
-            'entity' => $entity, 'form' => $form->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ];
     }
 
     /**
-     * Displays a form to edit an existing ImbalanPendaftaran entity.
-     *
      * @Route("/{id}/edit", name="rewardamount_edit")
      * @Template()
      */
-    public function editAction($id) {
-        $sekolah = $this->isRegisteredToSchool();
+    public function editAction($id)
+    {
         $this->setCurrentMenu();
 
         $em = $this->getDoctrine()->getManager();
@@ -168,24 +165,23 @@ class ImbalanPendaftaranController extends Controller
             throw $this->createNotFoundException('Entity ImbalanPendaftaran tak ditemukan.');
         }
 
-        $editForm = $this->createForm(new ImbalanPendaftaranType($this->container), $entity);
+        $editForm = $this->createForm('sisdik_imbalanpendaftaran', $entity);
         $deleteForm = $this->createDeleteForm($id);
 
-        return array(
-                'entity' => $entity, 'edit_form' => $editForm->createView(),
-                'delete_form' => $deleteForm->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ];
     }
 
     /**
-     * Edits an existing ImbalanPendaftaran entity.
-     *
      * @Route("/{id}/update", name="rewardamount_update")
      * @Method("POST")
      * @Template("LanggasSisdikBundle:ImbalanPendaftaran:edit.html.twig")
      */
-    public function updateAction(Request $request, $id) {
-        $sekolah = $this->isRegisteredToSchool();
+    public function updateAction(Request $request, $id)
+    {
         $this->setCurrentMenu();
 
         $em = $this->getDoctrine()->getManager();
@@ -197,52 +193,50 @@ class ImbalanPendaftaranController extends Controller
         }
 
         $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new ImbalanPendaftaranType($this->container), $entity);
+        $editForm = $this->createForm('sisdik_imbalanpendaftaran', $entity);
+
         $editForm->submit($request);
 
         if ($editForm->isValid()) {
-
             try {
                 $em->persist($entity);
                 $em->flush();
 
-                $this->get('session')->getFlashBag()
-                        ->add('success', $this->get('translator')->trans('flash.reward.amount.updated'));
-
+                $this
+                    ->get('session')
+                    ->getFlashBag()
+                    ->add('success', $this->get('translator')->trans('flash.reward.amount.updated'))
+                ;
             } catch (DBALException $e) {
                 $message = $this->get('translator')->trans('exception.unique.rewardamount');
                 throw new DBALException($message);
             }
 
-            return $this
-                    ->redirect(
-                            $this
-                                    ->generateUrl('rewardamount_edit',
-                                            array(
-                                                'id' => $id, 'page' => $this->getRequest()->get('page')
-                                            )));
+            return $this->redirect($this->generateUrl('rewardamount_edit', [
+                'id' => $id,
+            ]));
         }
 
-        return array(
-                'entity' => $entity, 'edit_form' => $editForm->createView(),
-                'delete_form' => $deleteForm->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ];
     }
 
     /**
-     * Deletes a ImbalanPendaftaran entity.
-     *
      * @Route("/{id}/delete", name="rewardamount_delete")
      * @Method("POST")
      */
-    public function deleteAction(Request $request, $id) {
-        $this->isRegisteredToSchool();
-
+    public function deleteAction(Request $request, $id)
+    {
         $form = $this->createDeleteForm($id);
+
         $form->submit($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+
             $entity = $em->getRepository('LanggasSisdikBundle:ImbalanPendaftaran')->find($id);
 
             if (!$entity) {
@@ -253,42 +247,49 @@ class ImbalanPendaftaranController extends Controller
                 $em->remove($entity);
                 $em->flush();
 
-                $this->get('session')->getFlashBag()
-                        ->add('success', $this->get('translator')->trans('flash.reward.amount.deleted'));
-
+                $this
+                    ->get('session')
+                    ->getFlashBag()
+                    ->add('success', $this->get('translator')->trans('flash.reward.amount.deleted'))
+                ;
             } catch (DBALException $e) {
                 $message = $this->get('translator')->trans('exception.delete.restrict');
                 throw new DBALException($message);
             }
         } else {
-            $this->get('session')->getFlashBag()
-                    ->add('error', $this->get('translator')->trans('flash.reward.amount.fail.delete'));
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add('error', $this->get('translator')->trans('flash.reward.amount.fail.delete'))
+            ;
         }
 
         return $this->redirect($this->generateUrl('rewardamount'));
     }
 
-    private function createDeleteForm($id) {
-        return $this->createFormBuilder(array(
-                    'id' => $id
-                ))->add('id', 'hidden')->getForm();
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder([
+                'id' => $id,
+            ])
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
     }
 
-    private function setCurrentMenu() {
+    private function setCurrentMenu()
+    {
+        $translator = $this->get('translator');
+
         $menu = $this->container->get('langgas_sisdik.menu.main');
-        $menu[$this->get('translator')->trans('headings.fee', array(), 'navigations')][$this->get('translator')->trans('links.reward.amount', array(), 'navigations')]->setCurrent(true);
+        $menu[$translator->trans('headings.fee', [], 'navigations')][$translator->trans('links.reward.amount', [], 'navigations')]->setCurrent(true);
     }
 
-    private function isRegisteredToSchool() {
-        $user = $this->getUser();
-        $sekolah = $user->getSekolah();
-
-        if (is_object($sekolah) && $sekolah instanceof Sekolah) {
-            return $sekolah;
-        } else if ($this->container->get('security.context')->isGranted('ROLE_SUPER_ADMIN')) {
-            throw new AccessDeniedException($this->get('translator')->trans('exception.useadmin'));
-        } else {
-            throw new AccessDeniedException($this->get('translator')->trans('exception.registertoschool'));
-        }
+    /**
+     * @return Sekolah
+     */
+    private function getSekolah()
+    {
+        return $this->getUser()->getSekolah();
     }
 }
--- src/Langgas/SisdikBundle/Form/ImbalanPendaftaranType.php
+++ src/Langgas/SisdikBundle/Form/ImbalanPendaftaranType.php
@@ -2,12 +2,15 @@
 
 namespace Langgas\SisdikBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
 use Langgas\SisdikBundle\Entity\Sekolah;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\SecurityContext;
 use JMS\DiExtraBundle\Annotation\FormType;
+use JMS\DiExtraBundle\Annotation\Inject;
+use JMS\DiExtraBundle\Annotation\InjectParams;
 
 /**
  * @FormType
@@ -15,101 +18,98 @@ use JMS\DiExtraBundle\Annotation\FormType;
 class ImbalanPendaftaranType extends AbstractType
 {
     /**
-     * @var ContainerInterface
+     * @var SecurityContext
      */
-    private $container;
+    private $securityContext;
 
     /**
-     * @param ContainerInterface $container
+     * @InjectParams({
+     *     "securityContext" = @Inject("security.context")
+     * })
+     *
+     * @param SecurityContext $securityContext
      */
-    public function __construct(ContainerInterface $container)
+    public function __construct(SecurityContext $securityContext)
     {
-        $this->container = $container;
+        $this->securityContext = $securityContext;
+    }
+
+    /**
+     * @return Sekolah
+     */
+    private function getSekolah()
+    {
+        return $this->securityContext->getToken()->getUser()->getSekolah();
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $user = $this->container
-            ->get('security.context')
-            ->getToken()
-            ->getUser()
-        ;
-        $sekolah = $user->getSekolah();
-
-        $em = $this->container->get('doctrine')->getManager();
-
-        if (is_object($sekolah) && $sekolah instanceof Sekolah) {
-            $querybuilder1 = $em->createQueryBuilder()
-                ->select('t')
-                ->from('LanggasSisdikBundle:Tahun', 't')
-                ->where('t.sekolah = :sekolah')
-                ->orderBy('t.tahun', 'DESC')
-                ->setParameter('sekolah', $sekolah)
-            ;
-            $builder
-                ->add('tahun', 'entity', [
-                    'class' => 'LanggasSisdikBundle:Tahun',
-                    'label' => 'label.year.entry',
-                    'multiple' => false,
-                    'expanded' => false,
-                    'property' => 'tahun',
-                    'empty_value' => false,
-                    'required' => true,
-                    'query_builder' => $querybuilder1,
-                    'attr' => [
-                        'class' => 'small',
-                    ],
-                ])
-            ;
-
-            $querybuilder2 = $em->createQueryBuilder()
-                ->select('t')
-                ->from('LanggasSisdikBundle:Gelombang', 't')
-                ->where('t.sekolah = :sekolah')
-                ->orderBy('t.urutan', 'ASC')
-                ->setParameter('sekolah', $sekolah)
-            ;
-            $builder
-                ->add('gelombang', 'entity', [
-                    'class' => 'LanggasSisdikBundle:Gelombang',
-                    'label' => 'label.admissiongroup.entry',
-                    'multiple' => false,
-                    'expanded' => false,
-                    'property' => 'nama',
-                    'empty_value' => false,
-                    'required' => true,
-                    'query_builder' => $querybuilder2,
-                    'attr' => [
-                        'class' => 'large',
-                    ],
-                ])
-            ;
-
-            $querybuilder3 = $em->createQueryBuilder()
-                ->select('t')
-                ->from('LanggasSisdikBundle:JenisImbalan', 't')
-                ->where('t.sekolah = :sekolah')
-                ->orderBy('t.nama', 'ASC')
-                ->setParameter('sekolah', $sekolah)
-            ;
-            $builder
-                ->add('jenisimbalan', 'entity', [
-                    'class' => 'LanggasSisdikBundle:JenisImbalan',
-                    'label' => 'label.reward.type.name',
-                    'multiple' => false,
-                    'expanded' => false,
-                    'property' => 'nama',
-                    'empty_value' => false,
-                    'required' => true,
-                    'query_builder' => $querybuilder3,
-                    'attr' => [
-                        'class' => 'medium',
-                    ],
-                ])
-            ;
-        }
+        $sekolah = $this->getSekolah();
 
         $builder
+            ->add('tahun', 'entity', [
+                'class' => 'LanggasSisdikBundle:Tahun',
+                'label' => 'label.year.entry',
+                'multiple' => false,
+                'expanded' => false,
+                'property' => 'tahun',
+                'empty_value' => false,
+                'required' => true,
+                'query_builder' => function (EntityRepository $repository) use ($sekolah) {
+                    $qb = $repository->createQueryBuilder('tahun')
+                        ->where('tahun.sekolah = :sekolah')
+                        ->orderBy('tahun.tahun', 'DESC')
+                        ->setParameter('sekolah', $sekolah)
+                    ;
+
+                    return $qb;
+                },
+                'attr' => [
+                    'class' => 'small',
+                ],
+            ])
+            ->add('gelombang', 'entity', [
+                'class' => 'LanggasSisdikBundle:Gelombang',
+                'label' => 'label.admissiongroup.entry',
+                'multiple' => false,
+                'expanded' => false,
+                'property' => 'nama',
+                'empty_value' => false,
+                'required' => true,
+                'query_builder' => function (EntityRepository $repository) use ($sekolah) {
+                    $qb = $repository->createQueryBuilder('gelombang')
+                        ->where('gelombang.sekolah = :sekolah')
+                        ->orderBy('gelombang.urutan', 'ASC')
+                        ->setParameter('sekolah', $sekolah)
+                    ;
+
+                    return $qb;
+                },
+                'attr' => [
+                    'class' => 'large',
+                ],
+            ])
+            ->add('jenisimbalan', 'entity', [
+                'class' => 'LanggasSisdikBundle:JenisImbalan',
+                'label' => 'label.reward.type.name',
+                'multiple' => false,
+                'expanded' => false,
+                'property' => 'nama',
+                'empty_value' => false,
+                'required' => true,
+                'query_builder' => function (EntityRepository $repository) use ($sekolah) {
+                    $qb = $repository->createQueryBuilder('jenisImbalan')
+                        ->where('jenisImbalan.sekolah = :sekolah')
+                        ->orderBy('jenisImbalan.nama', 'ASC')
+                        ->setParameter('sekolah', $sekolah)
+                    ;
+
+                    return $qb;
+                },
+                'attr' => [
+                    'class' => 'medium',
+                ],
+            ])
             ->add('nominal', 'money', [
                 'currency' => 'IDR',
                 'required' => true,
@@ -124,13 +124,15 @@ class ImbalanPendaftaranType extends AbstractType
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults([
-            'data_class' => 'Langgas\SisdikBundle\Entity\ImbalanPendaftaran',
-        ]);
+        $resolver
+            ->setDefaults([
+                'data_class' => 'Langgas\SisdikBundle\Entity\ImbalanPendaftaran',
+            ])
+        ;
     }
 
     public function getName()
     {
-        return 'langgas_sisdikbundle_imbalanpendaftarantype';
+        return 'sisdik_imbalanpendaftaran';
     }
 }
