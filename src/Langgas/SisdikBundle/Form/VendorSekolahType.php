1f5f6e711b44dc1d1e4f41118808b595a48961cd
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-08-28 20:30:24 +0700

    Vendor pihak ketiga yang digunakan oleh sekolah

---
+++ src/Langgas/SisdikBundle/Form/VendorSekolahType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Langgas\SisdikBundle\Form;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use JMS\DiExtraBundle\Annotation\FormType;
+
+/**
+ * @FormType
+ */
+class VendorSekolahType extends AbstractType
+{
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @param ObjectManager $objectManager
+     */
+    public function __construct(ObjectManager $objectManager) {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('sekolah', new EntityHiddenType($this->objectManager), [
+                'required' => true,
+                'class' => 'LanggasSisdikBundle:Sekolah',
+                'data' => $sekolah->getId(),
+            ])
+            ->add('urlPengirimPesan', 'text', [
+                'label' => 'label.url.pengirim.pesan',
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Langgas\SisdikBundle\Entity\VendorSekolah'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sisdik_vendorsekolah';
+    }
+}
