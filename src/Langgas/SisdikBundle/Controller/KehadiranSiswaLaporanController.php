b42bb4d9bea89a7dc9c3d2d0e15b3582bd2c0063
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-11-18 15:52:53 +0700

    Lihat laporan kehadiran siswa dengan fasilitas pencarian spesifik

--- src/Langgas/SisdikBundle/Controller/KehadiranSiswaLaporanController.php
+++ src/Langgas/SisdikBundle/Controller/KehadiranSiswaLaporanController.php
@@ -1,12 +1,14 @@
 <?php
 namespace Langgas\SisdikBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Langgas\SisdikBundle\Entity\TahunAkademik;
 use Langgas\SisdikBundle\Entity\Siswa;
 use Langgas\SisdikBundle\Entity\Kelas;
 use Langgas\SisdikBundle\Entity\Sekolah;
 use Langgas\SisdikBundle\Entity\KehadiranSiswa;
 use Langgas\SisdikBundle\Entity\JadwalKehadiran;
+use Langgas\SisdikBundle\Entity\SiswaKelas;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
@@ -30,13 +32,14 @@ class KehadiranSiswaLaporanController extends Controller
     /**
      * @Route("/", name="laporan-kehadiran-siswa")
      * @Method("GET")
-     * @Template("LanggasSisdikBundle:KehadiranSiswa:cari-laporan.html.twig")
+     * @Template("LanggasSisdikBundle:KehadiranSiswa:laporan.html.twig")
      */
     public function indexAction()
     {
         $sekolah = $this->getSekolah();
         $this->setCurrentMenu();
 
+        /* @var $em EntityManager */
         $em = $this->getDoctrine()->getManager();
 
         $searchform = $this->createForm('sisdik_kehadiransiswa_carilaporan');
@@ -63,11 +66,165 @@ class KehadiranSiswaLaporanController extends Controller
 
     /**
      * @Route("/lihat", name="laporan-kehadiran-siswa_lihat")
-     * @Method("POST")
-     * @Template("LanggasSisdikBundle:KehadiranSiswa:lihat-laporan.html.twig")
+     * @Method("GET")
+     * @Template("LanggasSisdikBundle:KehadiranSiswa:laporan.html.twig")
      */
     public function lihatAction()
     {
+        $sekolah = $this->getSekolah();
+        $this->setCurrentMenu();
+
+        /* @var $em EntityManager */
+        $em = $this->getDoctrine()->getManager();
+
+        $tahunAkademik = $em->getRepository('LanggasSisdikBundle:TahunAkademik')
+            ->findOneBy([
+                'aktif' => true,
+                'sekolah' => $sekolah->getId(),
+            ])
+        ;
+
+        if (!(is_object($tahunAkademik) && $tahunAkademik instanceof TahunAkademik)) {
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add('error', $this->get('translator')->trans('flash.tahun.akademik.tidak.ada.yang.aktif'))
+            ;
+
+            return $this->redirect($this->generateUrl('laporan-kehadiran-siswa'));
+        }
+
+        $siswaKelas = $em->createQueryBuilder()
+            ->select('siswaKelas, siswa, orangtuaWali')
+            ->from('LanggasSisdikBundle:SiswaKelas', 'siswaKelas')
+            ->leftJoin('siswaKelas.tahunAkademik', 'tahunAkademik')
+            ->leftJoin('siswaKelas.siswa', 'siswa')
+            ->leftJoin('siswa.orangtuaWali', 'orangtuaWali')
+            ->where('tahunAkademik.sekolah = :sekolah')
+            ->andWhere('siswaKelas.tahunAkademik = :tahunAkademik')
+            ->setParameter('sekolah', $sekolah)
+            ->setParameter('tahunAkademik', $tahunAkademik)
+            ->orderBy('siswa.nomorInduk', 'ASC')
+            ->addOrderBy('siswa.namaLengkap', 'ASC')
+        ;
+
+        $kehadiranSiswa = $em->createQueryBuilder()
+            ->select('kehadiranSiswa')
+            ->from('LanggasSisdikBundle:KehadiranSiswa', 'kehadiranSiswa')
+            ->where('kehadiranSiswa.sekolah = :sekolah')
+            ->andWhere('kehadiranSiswa.tahunAkademik = :tahunAkademik')
+            ->setParameter('sekolah', $sekolah)
+            ->setParameter('tahunAkademik', $tahunAkademik)
+        ;
+
+        $searchform = $this->createForm('sisdik_kehadiransiswa_carilaporan');
+        $searchform->submit($this->getRequest());
+        $searchdata = $searchform->getData();
+
+        if ($searchform->isValid()) {
+            if ($searchdata['kelas'] instanceof Kelas) {
+                $siswaKelas
+                    ->andWhere('siswaKelas.kelas = :kelas')
+                    ->setParameter('kelas', $searchdata['kelas'])
+                ;
+
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.kelas = :kelas')
+                    ->setParameter('kelas', $searchdata['kelas'])
+                ;
+            }
+
+            $dariTanggal = $searchdata['dariTanggal'];
+            $hinggaTanggal = $searchdata['hinggaTanggal'];
+
+            if ($dariTanggal instanceof \DateTime && $hinggaTanggal instanceof \DateTime) {
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.tanggal >= :dariTanggal AND kehadiranSiswa.tanggal <= :hinggaTanggal')
+                    ->setParameter('dariTanggal', $dariTanggal->format("Y-m-d 00:00:00"))
+                    ->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d 24:00:00"))
+                ;
+            } elseif (!($dariTanggal instanceof \DateTime) && $hinggaTanggal instanceof \DateTime) {
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal')
+                    ->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d"))
+                ;
+            } else {
+                $searchdata['hinggaTanggal'] = $hariIni = new \DateTime();
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal')
+                    ->setParameter('hinggaTanggal', $hariIni->format("Y-m-d"))
+                ;
+            }
+
+            if ($searchdata['searchkey'] != '') {
+                $siswaKelas
+                    ->andWhere("siswa.namaLengkap LIKE :searchkey OR siswa.nomorInduk LIKE :searchkey OR siswa.nomorIndukSistem = :searchkey2")
+                    ->setParameter('searchkey', "%{$searchdata['searchkey']}%")
+                    ->setParameter('searchkey2', $searchdata['searchkey'])
+                ;
+            }
+
+            if ($searchdata['statusKehadiran'] != '') {
+                $kehadiranSiswa
+                    ->andWhere("kehadiranSiswa.statusKehadiran = :statusKehadiran")
+                    ->setParameter('statusKehadiran', $searchdata['statusKehadiran'])
+                ;
+            }
+        } else {
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add('error', $this->get('translator')->trans('parameter.pencarian.laporan.tidak.boleh.kosong'))
+            ;
+
+            return $this->redirect($this->generateUrl('laporan-kehadiran-siswa'));
+        }
+
+        $daftarStatusKehadiran = JadwalKehadiran::getDaftarStatusKehadiran();
+        $daftarKehadiran = [];
+        $daftarSiswaDiKelas = $siswaKelas->getQuery()->getResult();
+        $kehadiranSiswaTotal = [];
+
+        foreach ($daftarSiswaDiKelas as $siswaDiKelas) {
+            $tmpKehadiran = [];
+            if ($siswaDiKelas instanceof SiswaKelas) {
+                $tmpKehadiran['siswa'] = $siswaDiKelas->getSiswa();
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.siswa = :siswa')
+                    ->setParameter('siswa', $siswaDiKelas->getSiswa())
+                ;
+                $kehadiranPerSiswa = $kehadiranSiswa->getQuery()->getResult();
+
+                foreach ($daftarStatusKehadiran as $key => $value) {
+                    $tmpJumlahStatus = 0;
+                    foreach ($kehadiranPerSiswa as $kehadiran) {
+                        if ($kehadiran instanceof KehadiranSiswa) {
+                            if ($key == $kehadiran->getStatusKehadiran()) {
+                                $tmpJumlahStatus++;
+                            }
+                        }
+                    }
+                    $tmpKehadiran[$key] = $tmpJumlahStatus;
+                    $kehadiranSiswaTotal[$key] = isset($kehadiranSiswaTotal[$key]) ? $kehadiranSiswaTotal[$key] + $tmpJumlahStatus : $tmpJumlahStatus;
+                }
+
+                $tmpKehadiran['jumlahHadir'] = $tmpKehadiran['a-hadir-tepat'] + $tmpKehadiran['b-hadir-telat'];
+                $tmpKehadiran['jumlahTidakHadir'] = $tmpKehadiran['c-alpa'] + $tmpKehadiran['d-izin'] + $tmpKehadiran['e-sakit'];
+                $daftarKehadiran[] = $tmpKehadiran;
+            }
+        }
+
+        return [
+            'searchkey' => $searchdata['searchkey'],
+            'searchform' => $searchform->createView(),
+            'kelas' => $searchdata['kelas'],
+            'tahunAkademik' => $tahunAkademik,
+            'dariTanggal' => $searchdata['dariTanggal'],
+            'hinggaTanggal' => $searchdata['hinggaTanggal'],
+            'daftarStatusKehadiran' => $daftarStatusKehadiran,
+            'kehadiranSiswa' => $daftarKehadiran,
+            'kehadiranSiswaTotal' => $kehadiranSiswaTotal,
+        ];
     }
 
     /**
@@ -100,12 +257,25 @@ class KehadiranSiswaLaporanController extends Controller
             ]);
         }
 
-        $querybuilder = $em->createQueryBuilder()
+        $siswaKelas = $em->createQueryBuilder()
+            ->select('siswaKelas, siswa')
+            ->from('LanggasSisdikBundle:SiswaKelas', 'siswaKelas')
+            ->leftJoin('siswaKelas.tahunAkademik', 'tahunAkademik')
+            ->leftJoin('siswaKelas.siswa', 'siswa')
+            ->where('tahunAkademik.sekolah = :sekolah')
+            ->andWhere('siswaKelas.tahunAkademik = :tahunAkademik')
+            ->setParameter('sekolah', $sekolah)
+            ->setParameter('tahunAkademik', $tahunAkademik)
+            ->orderBy('siswa.nomorInduk', 'ASC')
+            ->addOrderBy('siswa.namaLengkap', 'ASC')
+        ;
+
+        $kehadiranSiswa = $em->createQueryBuilder()
             ->select('kehadiranSiswa')
             ->from('LanggasSisdikBundle:KehadiranSiswa', 'kehadiranSiswa')
-            ->leftJoin('kehadiranSiswa.siswa', 'siswa')
             ->where('kehadiranSiswa.sekolah = :sekolah')
             ->andWhere('kehadiranSiswa.tahunAkademik = :tahunAkademik')
+            ->andWhere('kehadiranSiswa.siswa = :siswa')
             ->setParameter('sekolah', $sekolah)
             ->setParameter('tahunAkademik', $tahunAkademik)
         ;
@@ -116,34 +286,77 @@ class KehadiranSiswaLaporanController extends Controller
 
         if ($searchform->isValid()) {
             if ($searchdata['kelas'] instanceof Kelas) {
-                $querybuilder->andWhere('kehadiranSiswa.kelas = :kelas');
-                $querybuilder->setParameter('kelas', $searchdata['kelas']);
+                $siswaKelas
+                    ->andWhere('siswaKelas.kelas = :kelas')
+                    ->setParameter('kelas', $searchdata['kelas'])
+                ;
             }
 
             $dariTanggal = $searchdata['dariTanggal'];
             $hinggaTanggal = $searchdata['hinggaTanggal'];
 
             if ($dariTanggal instanceof \DateTime && $hinggaTanggal instanceof \DateTime) {
-                $querybuilder->andWhere('kehadiranSiswa.tanggal >= :dariTanggal AND kehadiranSiswa.tanggal <= :hinggaTanggal');
-                $querybuilder->setParameter('dariTanggal', $dariTanggal->format("Y-m-d 00:00:00"));
-                $querybuilder->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d 24:00:00"));
-                $querybuilder->addOrderBy('kehadiranSiswa.tanggal', 'ASC');
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.tanggal >= :dariTanggal AND kehadiranSiswa.tanggal <= :hinggaTanggal')
+                    ->setParameter('dariTanggal', $dariTanggal->format("Y-m-d 00:00:00"))
+                    ->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d 24:00:00"))
+                ;
             } elseif (!($dariTanggal instanceof \DateTime) && $hinggaTanggal instanceof \DateTime) {
-                $querybuilder->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal');
-                $querybuilder->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d"));
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal')
+                    ->setParameter('hinggaTanggal', $hinggaTanggal->format("Y-m-d"))
+                ;
             } else {
-                $hariIni = new \DateTime();
-                $querybuilder->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal');
-                $querybuilder->setParameter('hinggaTanggal', $hariIni->format("Y-m-d"));
+                $searchdata['hinggaTanggal'] = $hariIni = new \DateTime();
+                $kehadiranSiswa
+                    ->andWhere('kehadiranSiswa.tanggal = :hinggaTanggal')
+                    ->setParameter('hinggaTanggal', $hariIni->format("Y-m-d"))
+                ;
+            }
+        } else {
+            $return = [
+                "error" => $this->get('translator')->trans('parameter.pencarian.laporan.tidak.boleh.kosong'),
+            ];
+
+            $return = json_encode($return);
+
+            return new Response($return, 200, [
+                'Content-Type' => 'application/json',
+            ]);
+        }
+
+        $daftarStatusKehadiran = JadwalKehadiran::getDaftarStatusKehadiran();
+        $daftarKehadiran = [];
+        $daftarSiswaDiKelas = $siswaKelas->getQuery()->getResult();
+
+        foreach ($daftarSiswaDiKelas as $siswaDiKelas) {
+            $tmpKehadiran = [];
+            if ($siswaDiKelas instanceof SiswaKelas) {
+                $tmpKehadiran['siswa'] = $siswaDiKelas->getSiswa();
+                $kehadiranSiswa->setParameter('siswa', $siswaDiKelas->getSiswa());
+                $kehadiranPerSiswa = $kehadiranSiswa->getQuery()->getResult();
+
+                foreach ($daftarStatusKehadiran as $key => $value) {
+                    $tmpJumlahStatus = 0;
+                    foreach ($kehadiranPerSiswa as $kehadiran) {
+                        if ($kehadiran instanceof KehadiranSiswa) {
+                            if ($key == $kehadiran->getStatusKehadiran()) {
+                                $tmpJumlahStatus++;
+                            }
+                        }
+                    }
+                    $tmpKehadiran[$key] = $tmpJumlahStatus;
+                }
+
+                $tmpKehadiran['jumlahHadir'] = $tmpKehadiran['a-hadir-tepat'] + $tmpKehadiran['b-hadir-telat'];
+                $tmpKehadiran['jumlahTidakHadir'] = $tmpKehadiran['c-alpa'] + $tmpKehadiran['d-izin'] + $tmpKehadiran['e-sakit'];
+                $daftarKehadiran[] = $tmpKehadiran;
             }
         }
 
-        $querybuilder
-            ->addOrderBy('siswa.nomorInduk', 'ASC')
-            ->addOrderBy('siswa.namaLengkap', 'ASC')
-        ;
-
-        $kehadiranSiswa = $querybuilder->getQuery()->getResult();
+        return [
+            'searchform' => $searchform->createView(),
+        ];
 
         $documentbase = $this->get('kernel')->getRootDir() . self::DOCUMENTS_BASEDIR . self::BASEFILE;
         $outputdir = self::DOCUMENTS_OUTPUTDIR;
@@ -168,12 +381,12 @@ class KehadiranSiswaLaporanController extends Controller
                 $ziparchive = new \ZipArchive();
                 $ziparchive->open($documenttarget);
                 $ziparchive->addFromString('content.xml', $this->renderView("LanggasSisdikBundle:KehadiranSiswa:laporan.xml.twig", [
-                        'kehadiranSiswa' => $kehadiranSiswa,
-                        'tahunAkademik' => $tahunAkademik,
                         'kelas' => $searchdata['kelas'],
+                        'tahunAkademik' => $tahunAkademik,
                         'dariTanggal' => $searchdata['dariTanggal'],
                         'hinggaTanggal' => $searchdata['hinggaTanggal'],
-                        'daftarStatusKehadiran' => JadwalKehadiran::getDaftarStatusKehadiran(),
+                        'daftarStatusKehadiran' => $daftarStatusKehadiran,
+                        'kehadiranSiswa' => $daftarKehadiran,
                     ])
                 );
 
