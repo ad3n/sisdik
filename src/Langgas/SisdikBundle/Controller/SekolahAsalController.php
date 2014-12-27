5d86d45c2f119c5713d0e1af37a8e17eaa464a0c
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-12-27 11:31:39 +0700

    Merapikan kode controller sekolah asal

--- src/Langgas/SisdikBundle/Controller/SekolahAsalController.php
+++ src/Langgas/SisdikBundle/Controller/SekolahAsalController.php
@@ -1,83 +1,85 @@
 <?php
 
 namespace Langgas\SisdikBundle\Controller;
-use Langgas\SisdikBundle\Form\SimpleSearchFormType;
-use Symfony\Component\HttpFoundation\Response;
+
+use Doctrine\ORM\EntityManager;
 use Doctrine\DBAL\DBALException;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Langgas\SisdikBundle\Entity\SekolahAsal;
+use Langgas\SisdikBundle\Entity\Sekolah;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Langgas\SisdikBundle\Entity\SekolahAsal;
-use Langgas\SisdikBundle\Form\SekolahAsalType;
-use Langgas\SisdikBundle\Entity\Sekolah;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 
 /**
- * SekolahAsal controller.
- *
- * @Route("/sekolahasal")
+ * @Route("/sekolah-asal")
  * @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_KEPALA_SEKOLAH', 'ROLE_WAKIL_KEPALA_SEKOLAH', 'ROLE_PANITIA_PSB', 'ROLE_USER')")
  */
 class SekolahAsalController extends Controller
 {
-
     /**
-     * Lists all SekolahAsal entities.
-     *
      * @Route("/", name="sekolahasal")
      * @Method("GET")
      * @Template()
      * @Secure(roles="ROLE_ADMIN, ROLE_KEPALA_SEKOLAH, ROLE_WAKIL_KEPALA_SEKOLAH, ROLE_PANITIA_PSB")
      */
-    public function indexAction() {
-        $sekolah = $this->isRegisteredToSchool();
+    public function indexAction()
+    {
+        $sekolah = $this->getSekolah();
         $this->setCurrentMenu();
 
+        /* @var $em EntityManager */
         $em = $this->getDoctrine()->getManager();
 
-        $searchform = $this->createForm(new SimpleSearchFormType());
+        $searchform = $this->createForm('sisdik_cari');
 
-        $querybuilder = $em->createQueryBuilder()->select('t')->from('LanggasSisdikBundle:SekolahAsal', 't')
-                ->where('t.sekolah = :sekolah')->orderBy('t.nama', 'ASC')
-                ->setParameter('sekolah', $sekolah->getId());
+        $querybuilder = $em->createQueryBuilder()
+            ->select('sekolahAsal')
+            ->from('LanggasSisdikBundle:SekolahAsal', 'sekolahAsal')
+            ->where('sekolahAsal.sekolah = :sekolah')
+            ->orderBy('sekolahAsal.nama', 'ASC')
+            ->setParameter('sekolah', $sekolah)
+        ;
 
         $searchform->submit($this->getRequest());
         if ($searchform->isValid()) {
             $searchdata = $searchform->getData();
 
             if ($searchdata['searchkey'] != '') {
-                $querybuilder->andWhere('t.nama LIKE ?1 OR t.kode LIKE ?2 OR t.penghubung LIKE ?3');
-                $querybuilder->setParameter(1, "%{$searchdata['searchkey']}%");
-                $querybuilder->setParameter(2, "%{$searchdata['searchkey']}%");
-                $querybuilder->setParameter(3, "%{$searchdata['searchkey']}%");
+                $querybuilder
+                    ->andWhere('sekolahAsal.nama LIKE ?1 OR sekolahAsal.kode LIKE ?2 OR sekolahAsal.penghubung LIKE ?3')
+                    ->setParameter(1, "%{$searchdata['searchkey']}%")
+                    ->setParameter(2, "%{$searchdata['searchkey']}%")
+                    ->setParameter(3, "%{$searchdata['searchkey']}%")
+                ;
             }
         }
 
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($querybuilder, $this->getRequest()->query->get('page', 1));
 
-        return array(
-            'pagination' => $pagination, 'searchform' => $searchform->createView()
-        );
+        return [
+            'pagination' => $pagination,
+            'searchform' => $searchform->createView(),
+        ];
     }
+
     /**
-     * Creates a new SekolahAsal entity.
-     *
      * @Route("/", name="sekolahasal_create")
      * @Method("POST")
      * @Template("LanggasSisdikBundle:SekolahAsal:new.html.twig")
      * @Secure(roles="ROLE_ADMIN, ROLE_KEPALA_SEKOLAH, ROLE_WAKIL_KEPALA_SEKOLAH, ROLE_PANITIA_PSB")
      */
-    public function createAction(Request $request) {
-        $this->isRegisteredToSchool();
+    public function createAction(Request $request)
+    {
         $this->setCurrentMenu();
 
         $entity = new SekolahAsal();
-        $form = $this->createForm(new SekolahAsalType($this->container), $entity);
+        $form = $this->createForm('sisdik_sekolahasal', $entity);
         $form->submit($request);
 
         if ($form->isValid()) {
@@ -85,59 +87,106 @@ class SekolahAsalController extends Controller
             $em->persist($entity);
             $em->flush();
 
-            $this->get('session')->getFlashBag()
-                    ->add('success',
-                            $this->get('translator')
-                                    ->trans('flash.sekolah.asal.tersimpan',
-                                            array(
-                                                    '%nama%' => $entity->getNama(),
-                                                    '%kode%' => $entity->getKode()
-                                            )));
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add('success', $this->get('translator')->trans('flash.sekolah.asal.tersimpan', [
+                    '%nama%' => $entity->getNama(),
+                    '%kode%' => $entity->getKode(),
+                ]))
+            ;
 
-            return $this
-                    ->redirect(
-                            $this
-                                    ->generateUrl('sekolahasal_show',
-                                            array(
-                                                'id' => $entity->getId()
-                                            )));
+            return $this->redirect($this->generateUrl('sekolahasal_show', [
+                'id' => $entity->getId(),
+            ]));
         }
 
-        return array(
-            'entity' => $entity, 'form' => $form->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ];
     }
 
     /**
-     * Displays a form to create a new SekolahAsal entity.
-     *
      * @Route("/new", name="sekolahasal_new")
      * @Method("GET")
      * @Template()
      * @Secure(roles="ROLE_ADMIN, ROLE_KEPALA_SEKOLAH, ROLE_WAKIL_KEPALA_SEKOLAH, ROLE_PANITIA_PSB")
      */
-    public function newAction() {
-        $this->isRegisteredToSchool();
+    public function newAction()
+    {
         $this->setCurrentMenu();
 
         $entity = new SekolahAsal();
-        $form = $this->createForm(new SekolahAsalType($this->container), $entity);
+        $form = $this->createForm('sisdik_sekolahasal', $entity);
 
-        return array(
-            'entity' => $entity, 'form' => $form->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ];
     }
 
     /**
-     * Finds and displays a SekolahAsal entity.
-     *
+     * @param Request $request
+     * @Route("/ambil", name="sekolahasal_ajax_ambilnama")
+     * @Method("GET")
+     */
+    public function ajaxGetSekolahAsal(Request $request)
+    {
+        $sekolah = $this->getSekolah();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $filter = $this->getRequest()->query->get('filter');
+        $id = $this->getRequest()->query->get('id');
+
+        $querybuilder = $em->createQueryBuilder()
+            ->select('sekolahAsal')
+            ->from('LanggasSisdikBundle:SekolahAsal', 'sekolahAsal')
+            ->where('sekolahAsal.sekolah = :sekolah')
+            ->orderBy('sekolahAsal.nama', 'ASC')
+            ->setParameter('sekolah', $sekolah)
+        ;
+
+        if ($id != '') {
+            $querybuilder
+                ->andWhere('sekolahAsal.id = :id')
+                ->setParameter('id', $id)
+            ;
+        } else {
+            $querybuilder
+                ->andWhere('sekolahAsal.nama LIKE ?1 OR sekolahAsal.kode LIKE ?2')
+                ->setParameter(1, "%$filter%")
+                ->setParameter(2, "%$filter%")
+            ;
+        }
+
+        $results = $querybuilder->getQuery()->getResult();
+
+        $retval = [];
+        foreach ($results as $result) {
+            if ($result instanceof SekolahAsal) {
+                $retval[] = [
+                    'id' => $result->getId(),
+                    'label' =>/** @Ignore */ $result->getNama(),
+                    'value' => $result->getNama(),
+                ];
+            }
+        }
+
+        return new Response(json_encode($retval), 200, [
+            'Content-Type' => 'application/json',
+        ]);
+    }
+
+    /**
      * @Route("/{id}", name="sekolahasal_show")
      * @Method("GET")
      * @Template()
      * @Secure(roles="ROLE_ADMIN, ROLE_KEPALA_SEKOLAH, ROLE_WAKIL_KEPALA_SEKOLAH, ROLE_PANITIA_PSB")
      */
-    public function showAction($id) {
-        $this->isRegisteredToSchool();
+    public function showAction($id)
+    {
         $this->setCurrentMenu();
 
         $em = $this->getDoctrine()->getManager();
@@ -150,21 +199,20 @@ class SekolahAsalController extends Controller
 
         $deleteForm = $this->createDeleteForm($id);
 
-        return array(
-            'entity' => $entity, 'delete_form' => $deleteForm->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ];
     }
 
     /**
-     * Displays a form to edit an existing SekolahAsal entity.
-     *
      * @Route("/{id}/edit", name="sekolahasal_edit")
      * @Method("GET")
      * @Template()
      * @Secure(roles="ROLE_ADMIN, ROLE_KEPALA_SEKOLAH, ROLE_WAKIL_KEPALA_SEKOLAH, ROLE_PANITIA_PSB")
      */
-    public function editAction($id) {
-        $this->isRegisteredToSchool();
+    public function editAction($id)
+    {
         $this->setCurrentMenu();
 
         $em = $this->getDoctrine()->getManager();
@@ -175,25 +223,24 @@ class SekolahAsalController extends Controller
             throw $this->createNotFoundException('Entity SekolahAsal tak ditemukan.');
         }
 
-        $editForm = $this->createForm(new SekolahAsalType($this->container), $entity);
+        $editForm = $this->createForm('sisdik_sekolahasal', $entity);
         $deleteForm = $this->createDeleteForm($id);
 
-        return array(
-                'entity' => $entity, 'edit_form' => $editForm->createView(),
-                'delete_form' => $deleteForm->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ];
     }
 
     /**
-     * Edits an existing SekolahAsal entity.
-     *
      * @Route("/{id}", name="sekolahasal_update")
      * @Method("POST")
      * @Template("LanggasSisdikBundle:SekolahAsal:edit.html.twig")
      * @Secure(roles="ROLE_ADMIN, ROLE_KEPALA_SEKOLAH, ROLE_WAKIL_KEPALA_SEKOLAH, ROLE_PANITIA_PSB")
      */
-    public function updateAction(Request $request, $id) {
-        $this->isRegisteredToSchool();
+    public function updateAction(Request $request, $id)
+    {
         $this->setCurrentMenu();
 
         $em = $this->getDoctrine()->getManager();
@@ -205,46 +252,41 @@ class SekolahAsalController extends Controller
         }
 
         $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new SekolahAsalType($this->container), $entity);
+        $editForm = $this->createForm('sisdik_sekolahasal', $entity);
         $editForm->submit($request);
 
         if ($editForm->isValid()) {
             $em->persist($entity);
             $em->flush();
 
-            $this->get('session')->getFlashBag()
-                    ->add('success',
-                            $this->get('translator')
-                                    ->trans('flash.sekolah.asal.terbarui',
-                                            array(
-                                                    '%nama%' => $entity->getNama(),
-                                                    '%kode%' => $entity->getKode()
-                                            )));
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add('success', $this->get('translator')->trans('flash.sekolah.asal.terbarui', [
+                    '%nama%' => $entity->getNama(),
+                    '%kode%' => $entity->getKode(),
+                ]))
+            ;
 
-            return $this
-                    ->redirect(
-                            $this
-                                    ->generateUrl('sekolahasal_edit',
-                                            array(
-                                                'id' => $id
-                                            )));
+            return $this->redirect($this->generateUrl('sekolahasal_edit', [
+                'id' => $id,
+            ]));
         }
 
-        return array(
-                'entity' => $entity, 'edit_form' => $editForm->createView(),
-                'delete_form' => $deleteForm->createView(),
-        );
+        return [
+            'entity' => $entity,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ];
     }
+
     /**
-     * Deletes a SekolahAsal entity.
-     *
      * @Route("/{id}/delete", name="sekolahasal_delete")
      * @Method("POST")
      * @Secure(roles="ROLE_ADMIN, ROLE_KEPALA_SEKOLAH, ROLE_WAKIL_KEPALA_SEKOLAH, ROLE_PANITIA_PSB")
      */
-    public function deleteAction(Request $request, $id) {
-        $this->isRegisteredToSchool();
-
+    public function deleteAction(Request $request, $id)
+    {
         $form = $this->createDeleteForm($id);
         $form->submit($request);
 
@@ -260,104 +302,60 @@ class SekolahAsalController extends Controller
                 $em->remove($entity);
                 $em->flush();
 
-                $this->get('session')->getFlashBag()
-                        ->add('success',
-                                $this->get('translator')
-                                        ->trans('flash.sekolah.asal.terhapus',
-                                                array(
-                                                        '%nama%' => $entity->getNama(),
-                                                        '%kode%' => $entity->getKode()
-                                                )));
+                $this
+                    ->get('session')
+                    ->getFlashBag()
+                    ->add('success', $this->get('translator')->trans('flash.sekolah.asal.terhapus', [
+                        '%nama%' => $entity->getNama(),
+                        '%kode%' => $entity->getKode(),
+                    ]))
+                ;
             } catch (DBALException $e) {
                 $message = $this->get('translator')->trans('exception.delete.restrict');
                 throw new DBALException($message);
             }
         } else {
-            $this->get('session')->getFlashBag()
-                    ->add('error',
-                            $this->get('translator')
-                                    ->trans('flash.sekolah.asal.gagal.dihapus',
-                                            array(
-                                                    '%nama%' => $entity->getNama(),
-                                                    '%kode%' => $entity->getKode()
-                                            )));
+            $this
+                ->get('session')
+                ->getFlashBag()
+                ->add('error', $this->get('translator')->trans('flash.sekolah.asal.gagal.dihapus', [
+                    '%nama%' => $entity->getNama(),
+                    '%kode%' => $entity->getKode(),
+                ]))
+            ;
         }
 
         return $this->redirect($this->generateUrl('sekolahasal'));
     }
 
     /**
-     * Mengambil sekolah asal menggunakan kotak autocomplete
+     * @param mixed $id
      *
-     * @param Request $request
-     * @Route("/ajax/ambilsekolah", name="sekolahasal_ajax_ambilnama")
+     * @return \Symfony\Component\Form\Form
      */
-    public function ajaxGetSekolahAsal(Request $request) {
-        $sekolah = $this->isRegisteredToSchool();
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder([
+                'id' => $id,
+            ])
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
 
-        $em = $this->getDoctrine()->getManager();
+    private function setCurrentMenu()
+    {
+        $translator = $this->get('translator');
 
-        $filter = $this->getRequest()->query->get('filter');
-        $id = $this->getRequest()->query->get('id');
-
-        $querybuilder = $em->createQueryBuilder()->select('t')->from('LanggasSisdikBundle:SekolahAsal', 't')
-                ->where('t.sekolah = :sekolah')->orderBy('t.nama', 'ASC')
-                ->setParameter('sekolah', $sekolah->getId());
-
-        if ($id != '') {
-            $querybuilder->andWhere('t.id = :id');
-            $querybuilder->setParameter('id', $id);
-        } else {
-            $querybuilder->andWhere('t.nama LIKE ?1 OR t.kode LIKE ?2');
-            $querybuilder->setParameter(1, "%$filter%");
-            $querybuilder->setParameter(2, "%$filter%");
-        }
-
-        $results = $querybuilder->getQuery()->getResult();
-
-        $retval = array();
-        foreach ($results as $result) {
-            if ($result instanceof SekolahAsal) {
-                $retval[] = array(
-                    'id' => $result->getId(), 'label' => /** @Ignore */ $result->getNama(), 'value' => $result->getNama(),
-                );
-            }
-        }
-
-        return new Response(json_encode($retval), 200,
-                array(
-                    'Content-Type' => 'application/json'
-                ));
+        $menu = $this->container->get('langgas_sisdik.menu.main');
+        $menu[$translator->trans('headings.pendaftaran', [], 'navigations')][$translator->trans('links.sekolahasal', [], 'navigations')]->setCurrent(true);
     }
 
     /**
-     * Creates a form to delete a SekolahAsal entity by id.
-     *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
+     * @return Sekolah
      */
-    private function createDeleteForm($id) {
-        return $this->createFormBuilder(array(
-                    'id' => $id
-                ))->add('id', 'hidden')->getForm();
-    }
-
-    private function setCurrentMenu() {
-        $menu = $this->container->get('langgas_sisdik.menu.main');
-        $menu[$this->get('translator')->trans('headings.pendaftaran', array(), 'navigations')][$this->get('translator')->trans('links.sekolahasal', array(), 'navigations')]->setCurrent(true);
-    }
-
-    private function isRegisteredToSchool() {
-        $user = $this->getUser();
-        $sekolah = $user->getSekolah();
-
-        if (is_object($sekolah) && $sekolah instanceof Sekolah) {
-            return $sekolah;
-        } elseif ($this->container->get('security.context')->isGranted('ROLE_SUPER_ADMIN')) {
-            throw new AccessDeniedException($this->get('translator')->trans('exception.useadmin'));
-        } else {
-            throw new AccessDeniedException($this->get('translator')->trans('exception.registertoschool'));
-        }
+    private function getSekolah()
+    {
+        return $this->getUser()->getSekolah();
     }
 }
