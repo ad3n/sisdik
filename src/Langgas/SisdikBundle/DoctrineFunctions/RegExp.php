72cef1d1a0b4da6a00fa76132d9d6bd474a395b8
Ihsan Faisal <ihsanfaisal@gmail.com>
2015-01-02 08:17:13 +0700

    Menambah fungsi regular expression untuk doctrine

---
+++ src/Langgas/SisdikBundle/DoctrineFunctions/RegExp.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Langgas\SisdikBundle\DoctrineFunctions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+class RegExp extends FunctionNode
+{
+    public $regexpExpression = null;
+    public $valueExpression = null;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->regexpExpression = $parser->StringPrimary();
+        $parser->match(Lexer::T_COMMA);
+
+        $this->valueExpression = $parser->StringExpression();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return '('.$this->valueExpression->dispatch($sqlWalker).' REGEXP '.$sqlWalker->walkStringPrimary($this->regexpExpression).')';
+    }
+}
