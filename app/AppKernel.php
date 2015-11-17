29d64245934c5f704953a7aaa20e211f800fb3be
Ihsan Faisal <ihsanfaisal@gmail.com>
2015-11-17 12:31:00 +0700

    Memindahkan folder cache dan logs ke /dev/shm

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -49,4 +49,22 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader) {
         $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
     }
+
+    public function getCacheDir()
+    {
+        if (in_array($this->environment, array('dev', 'test'))) {
+            return '/dev/shm/sisdik/cache/' .  $this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir()
+    {
+        if (in_array($this->environment, array('dev', 'test'))) {
+            return '/dev/shm/sisdik/logs';
+        }
+
+        return parent::getLogDir();
+    }
 }
